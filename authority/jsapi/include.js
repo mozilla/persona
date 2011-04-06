89d8786e796722a1b875328845abe401893c1044
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-04-06 12:40:59 -0600

    plug the RP into authority

--- authority/jsapi/include.js
+++ authority/jsapi/include.js
@@ -1,4 +1,10 @@
 // this is the file that the RP includes to shim in the
 // navigator.id.getVerifiedEmail() function
 
+if (!navigator.id) { navigator.id = {} }
 
+if (!navigator.id.getVerifiedEmail) {
+    navigator.id.getVerifiedEmail = function() {
+        alert("getVerifiedEmail invoked");
+    };
+}
