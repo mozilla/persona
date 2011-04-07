6f3cdd40c9761ab36d8678f01eddf7685ddcd884
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-04-06 21:09:06 -0600

    conduit successfully established and bi-directional messaging works

--- authority/jsapi/picker.js
+++ authority/jsapi/picker.js
@@ -1,3 +1,15 @@
 // this is the picker code!  it runs in the identity provider's domain, and
 // fiddles the dom expressed by picker.html
 
+var chan = Channel.build(
+    {
+        window: window.opener,
+        origin: "*",
+        scope: "mozid"
+
+    });
+chan.bind("getVerifiedEmail", function(trans, s) {
+    alert("you called getVerifiedEmail");
+    return "AAA";
+});
+
