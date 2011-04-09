3830e10e2481c1a45cfc124edbac27425a1817db
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-04-09 11:50:27 -0600

    fix 2 bugs

--- authority/server/email.js
+++ authority/server/email.js
@@ -1,9 +1,11 @@
+const db = require('./db');
+
 exports.sendVerificationEmail = function(email, secret) {
   console.log("fakely sending a verification email for " + email);
   // XXX: what we would really do here is send out an email, instead
   // we'll just wait 5 seconds and manually feed the secret back into the
   // system, as if a user had clicked a link
-  setTiemout(function() {
+  setTimeout(function() {
     db.gotVerificationSecret(secret);
   }, 5000);
 };
