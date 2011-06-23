986c7fa7723ea12e897f3e9a07686c53fa4fc330
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-06-23 16:40:09 -0600

    remove an obsolete comment referring to work that's already done

--- browserid/static/include.js
+++ browserid/static/include.js
@@ -649,11 +649,6 @@ if (!navigator.id.getVerifiedEmail || navigator.id._getVerifiedEmailIsShimmed)
   };
 
   navigator.id.registerVerifiedEmail = function(email, onsuccess, onerror) {
-
-    // TODO: We really don't need to display a dialog for this.
-    // I'm showing one just because it's easier than figuring out
-    // how to embed an iframe in this case.
-
     var doc = window.document;
     iframe = _create_iframe(doc);
     if (chan) chan.destroy();
