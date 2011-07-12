5ee90d46899e51f71bb12a81e604d1ad3ae916d8
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-07-11 21:43:24 -0600

    s/log in/sign in/

--- browserid/static/dialog/main.js
+++ browserid/static/dialog/main.js
@@ -230,8 +230,8 @@
             $("#authenticate_dialog div.attention_lame").hide().fadeIn(400);
           } else {
             runWaitingDialog(
-              "Finishing Log In...",
-              "In just a moment you'll be logged into BrowserID.",
+              "Finishing Sign In...",
+              "In just a moment you'll be signed into BrowserID.",
               onsuccess, onerror);
 
             syncIdentities(onsuccess, onerror);
