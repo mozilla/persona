814c3a8c051428536499dfb95ac573b78401f58f
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-07-18 16:18:12 -0600

    consistent indenting

--- browserid/static/dialog/dialog.js
+++ browserid/static/dialog/dialog.js
@@ -9,28 +9,28 @@ steal.plugins(
 	.css("style")	// loads styles
 
 	.resources('jschannel',
-             'underscore-min',
-             'crypto',
-             'crypto-api',
-             'channel',
-             'storage')					// 3rd party script's (like jQueryUI), in resources folder
+               'underscore-min',
+               'crypto',
+               'crypto-api',
+               'channel',
+               'storage')					// 3rd party script's (like jQueryUI), in resources folder
 
 	.models()						// loads files in models folder 
 
 	.controllers('dialog')					// loads files in controllers folder
 
 	.views('authenticate.ejs',
-            'addemail.ejs',
-            'body.ejs',
-            'bottom-addemail.ejs',
-            'bottom-confirmemail.ejs',
-            'bottom-continue.ejs',
-            'bottom-pickemail.ejs',
-            'bottom-signin.ejs',
-            'bottom.ejs',
-            'confirmemail.ejs',
-            'create.ejs',
-            'forgotpassword.ejs',
-            'signin.ejs',
-            'wait.ejs'
-            );						// adds views to be added to build
+           'addemail.ejs',
+           'body.ejs',
+           'bottom-addemail.ejs',
+           'bottom-confirmemail.ejs',
+           'bottom-continue.ejs',
+           'bottom-pickemail.ejs',
+           'bottom-signin.ejs',
+           'bottom.ejs',
+           'confirmemail.ejs',
+           'create.ejs',
+           'forgotpassword.ejs',
+           'signin.ejs',
+           'wait.ejs'
+          );						// adds views to be added to build
