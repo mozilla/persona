7e0b8e894081ff69da12d8d2d427f53b58a979c9
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-07-22 12:30:42 -0600

    emit better error messages when uglifyjs or java are missing

--- browserid/compress.sh
+++ browserid/compress.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+UGLIFY=`which uglifyjs 2> /dev/null`
+if [ ! -x "$UGLIFY" ]; then
+    echo "uglifyjs not found in your path.  can't create production resources.  disaster."
+    exit 1
+fi
+
+JAVA=`which java 2> /dev/null`
+if [ ! -x "$UGLIFY" ]; then
+    echo "java not found in your path.  can't create production resources.  disaster."
+    exit 1
+fi
+
+
+
 YUI_LOCATION='../../static/steal/build/scripts/yui.jar'
 
 echo ''
@@ -10,7 +24,7 @@ cd static
 steal/js dialog/scripts/build.js
 
 cd dialog
-uglifyjs < production.js > production.min.js
+$UGLIFY < production.js > production.min.js
 mv production.min.js production.js
 
 echo ''
@@ -20,8 +34,8 @@ echo ''
 cd ../js
 # re-minimize everything together
 cat jquery-1.6.2.min.js ../dialog/resources/underscore-min.js browserid.js > lib.js
-uglifyjs < lib.js > lib.min.js
+$UGLIFY < lib.js > lib.min.js
 
 cd ../css
 cat github.css style.css > browserid.css
-java -jar $YUI_LOCATION browserid.css -o browserid.min.css
+$JAVA -jar $YUI_LOCATION browserid.css -o browserid.min.css
