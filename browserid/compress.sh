ce081be059baeef4f277d1aceedf9747ad4630eb
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-07-22 12:15:20 -0600

    use uglify for javascript.

--- browserid/compress.sh
+++ browserid/compress.sh
@@ -10,7 +10,7 @@ cd static
 steal/js dialog/scripts/build.js
 
 cd dialog
-java -jar $YUI_LOCATION production.js -o production.min.js
+uglifyjs < production.js > production.min.js
 mv production.min.js production.js
 
 echo ''
@@ -18,15 +18,10 @@ echo '****Building BrowserID.org HTML, CSS, and JS****'
 echo ''
 
 cd ../js
-# Yes, this is the right way of doing this.  We are compressing ONLY 
-# browserid.js because both jquery and underscore are already compressed.  Then 
-# take the three of them, concatinate them together, and create lib.min.js
-java -jar $YUI_LOCATION browserid.js -o browserid.min.js
-cat jquery-1.6.2.min.js ../dialog/resources/underscore-min.js browserid.min.js > lib.min.js
+# re-minimize everything together
+cat jquery-1.6.2.min.js ../dialog/resources/underscore-min.js browserid.js > lib.js
+uglifyjs < lib.js > lib.min.js
 
 cd ../css
 cat github.css style.css > browserid.css
 java -jar $YUI_LOCATION browserid.css -o browserid.min.css
-
-
-
