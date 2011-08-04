2164001b4166218a798b597161e699b82e9ef24e
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-08-04 17:50:02 -0600

    stub out mysql driver

---
+++ browserid/lib/db_mysql.js
@@ -0,0 +1,57 @@
+
+
+exports.open = function() {
+  throw "not implemented";
+};
+
+exports.close = function() {
+  throw "not implemented";
+};
+
+exports.emailKnown = function() {
+  throw "not implemented";
+}
+
+exports.isStaged = function() {
+  throw "not implemented";
+}
+
+exports.emailsBelongToSameAccount = function() {
+  throw "not implemented";
+}
+
+exports.addKeyToEmail = function() {
+  throw "not implemented";
+}
+
+exports.stageUser = function() {
+  throw "not implemented";
+}
+
+exports.stageEmail = function() {
+  throw "not implemented";
+}
+
+exports.gotVerificationSecret = function() {
+  throw "not implemented";
+}
+
+exports.checkAuth = function() {
+  throw "not implemented";
+}
+
+exports.getSyncResponse = function() {
+  throw "not implemented";
+}
+
+exports.pubkeysForEmail = function() {
+  throw "not implemented";
+}
+
+exports.removeEmail = function() {
+  throw "not implemented";
+}
+
+exports.cancelAccount = function() {
+  throw "not implemented";
+}
