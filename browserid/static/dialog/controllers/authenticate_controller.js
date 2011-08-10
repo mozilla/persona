da1563c0048eb05313ac4052e185fe62ccea2c3a
Shane Tomlinson <stomlinson@mozilla.com>
2011-08-10 12:45:05 -0700

    Formatting.

--- browserid/static/dialog/controllers/authenticate_controller.js
+++ browserid/static/dialog/controllers/authenticate_controller.js
@@ -2,41 +2,41 @@
   "use strict";
 
   PageController.extend("Authenticate", {}, {
-      init: function() {
-        this._super({
-          bodyTemplate: "authenticate.ejs",
-          bodyVars: {
-            sitename: BrowserIDNetwork.origin
-          },
-          footerTemplate: "bottom-signin.ejs",
-          footerVars: {}
-        });
-      },
+    init: function() {
+      this._super({
+        bodyTemplate: "authenticate.ejs",
+        bodyVars: {
+          sitename: BrowserIDNetwork.origin
+        },
+        footerTemplate: "bottom-signin.ejs",
+        footerVars: {}
+      });
+    },
 
-      validate: function() {
-        var email = $("#email_input").val();
-        var pass = $("#password_input").val();
+    validate: function() {
+      var email = $("#email_input").val();
+      var pass = $("#password_input").val();
 
-        return true;
-      },
+      return true;
+    },
 
-      submit: function() {
-        var email = $("#email_input").val();
-        var pass = $("#password_input").val();
+    submit: function() {
+      var email = $("#email_input").val();
+      var pass = $("#password_input").val();
 
-        var self = this;
-        BrowserIDNetwork.authenticate(email, pass, function(authenticated) {
-          if (authenticated) {
-            self.doWait(BrowserIDWait.authentication);
-            self.close("authenticate:authenticated");
-          } else {
-            self.find("#nosuchaccount").hide().fadeIn(400);
-          }
-        }, function(resp) {
-          self.runErrorDialog(BrowserIDErrors.authentication);
-          self.close("cancel");
-        });
-      }
+      var self = this;
+      BrowserIDNetwork.authenticate(email, pass, function(authenticated) {
+        if (authenticated) {
+          self.doWait(BrowserIDWait.authentication);
+          self.close("authenticate:authenticated");
+        } else {
+          self.find("#nosuchaccount").hide().fadeIn(400);
+        }
+      }, function(resp) {
+        self.runErrorDialog(BrowserIDErrors.authentication);
+        self.close("cancel");
+      });
+    }
   });
 
 }());
