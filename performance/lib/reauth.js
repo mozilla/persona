e499424f4b46f97d5580f80005fdc1ae68c096a4
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-08-17 14:54:35 +0300

    (load gen) document the network transactions that occur during the 'reauth' activity, which simulates a user with an active browserid session using browserid to log into a site

--- performance/lib/reauth.js
+++ performance/lib/reauth.js
@@ -1,4 +1,49 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla BrowserID.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Lloyd Hilaiel <lloyd@hilaiel.com> 
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/* this file is the "reauth" activity, which simulates the process of a user
+ * browserid who is logged into browserid with an authenticated session
+ * using browserid at an RP to log in */
+
 exports.startFunc = function(cfg, cb) {
-  // XXX: write me
+
+  // 1. RP includes include.js 
+  // 2. users' browser loads all code associated with dialog
+  // 3. in page javascript calls CSRF to get a CSRF token
+  // 4. the RP will call /verify to verify a generated assertion
+
   setTimeout(function() { cb(true); }, 10); 
 };
