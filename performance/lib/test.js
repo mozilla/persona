c6c0196b846eb578227a45ba75c70bd27e2673e3
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-08-19 14:19:30 +0300

    create user_db which is capable of generating random users who visit random sites, with random email addresses and random passwords

--- performance/lib/test.js
+++ performance/lib/test.js
@@ -1,4 +1,6 @@
-const wcli = require("./wsapi_client.js");
+const
+wcli = require("./wsapi_client.js"),
+userdb = require("./user_db.js");
 
 // lets create a user!
 
@@ -8,17 +10,25 @@ var ctx = {};
 // and a configuration (what server we're talking to)
 var cfg = { browserid: 'http://127.0.0.1:10002' }
 
+// and a user
+var user = userdb.getNewUser();
+userdb.addKeyToUser(user);
+
 // now start the dance with a call to stage_user
 wcli.post(cfg, '/wsapi/stage_user', ctx, {
-  email: 'first@fakeemail.com',
-  pass: 'firstfakepass',
-  pubkey: 'fakepubkey',
-  site:'fakesite.com'
+  email: user.emails[0],
+  pass: user.password,
+  pubkey: user.pubkeys[0],
+  site: user.sites[0]
 }, function (r) {
   // now get the verification secret
   wcli.get(cfg, '/wsapi/fake_verification', ctx, {
-    email: "first@fakeemail.com"
+    email: user.emails[0]
   }, function (r) {
-    console.log(r);
+    wcli.get(cfg, '/wsapi/prove_email_ownership', ctx, {
+      token: r.body
+    }, function (r) {
+      console.log(r.body);
+    });
   });
 });
--- performance/lib/user_db.js
+++ performance/lib/user_db.js
@@ -34,35 +34,56 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-/* this file is the "signup" activity, which simulates the process of a new user
- * signing up for browserid. */
-exports.startFunc = function(cfg, cb) {
+/* the "user database".  a little in-memory collection of users for the
+ * purposes of performance testing. 
+ */
 
-  // A new user signing up for browserid looks like this in terms of
-  // network transactions:
-  //
-  // 1. RP includes include.js 
-  // 2. users' browser loads all code associated with dialog
-  // 3. in page javascript calls CSRF to get a CSRF token
-  // 4. /wsapi/have_email is called some number of times to give feedback as the
-  //    user types in their email address
-  // 5. /wsapi/stage_user is called to stage the user for creation when the
-  //    user clicks "continue" after having chosen a password
-  // 6. /wsapi/registration_status is called once for each second it takes the
-  //    user to go to their email and click on the verification link
-  // 6.5. in the load testing environment, we make a call to the server to get
-  //    the email verification token
-  // 7. /prove is called on the server, passing in the authentication token
-  // 8. /manage is called on the server as the user's page transitions from
-  //    the verify screen to the manage screen.
-  // 9. /wsapi/sync_emails is called from the client (XXX: this is probably a bug,
-  //    there's no utility to this)
-  // 10. /wsapi/set_key is called from the client to inform the server of the
-  //    user's public key (XXX: this will go away when we migrate to certificates
-  //    and instead, the server will be asked to sign the user's public key.)
-  // 11.  the RP will call /verify to verify a generated assertion
+var secrets = require('../../libs/secrets.js');
 
-  // XXX: write me
+// the grandiose database
+var users = [ ];
 
-  setTimeout(function() { cb(true); }, 10); 
+exports.numUsers = function() { return users.length; }
+
+function genEmail() {
+  return secrets.generate(8) + "@" + secrets.generate(8) + "." + secrets.generate(3);
+}
+
+exports.getNewUser = function() {
+  // create and return a new user record
+  var user = {
+    // all users start with a single email address
+    emails: [ genEmail() ],
+    // a password of 10 chars
+    password: secrets.generate(10),
+    // and four sites that they visit
+    sites: [
+      secrets.generate(8) + "." + secrets.generate(3),
+      secrets.generate(8) + "." + secrets.generate(3),
+      secrets.generate(8) + "." + secrets.generate(3),
+      secrets.generate(8) + "." + secrets.generate(3)
+    ],
+    // and no public keys (XXX: beware the cometh of certs)
+    pubkeys: [
+    ]
+  };
+  users.push(user);
+  return user;
 };
+
+exports.getExistingUser = function() {
+  if (!users.length) throw "can't get an existing user. there aren't any.  call getNewUser first.";
+  return users[Math.floor(Math.random()*users.length)];
+};
+
+exports.addEmailToUser = function(user) {
+  var email = genEmail();
+  user.emails.push(email);
+  return email;
+};
+
+exports.addKeyToUser = function(user) {
+  var key = secrets.generate(128);
+  user.pubkeys.push(key);
+  return key;
+}
