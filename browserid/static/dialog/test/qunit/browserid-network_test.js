5eea3ce01dea8739532cdecb8b75066ebe759d2f
Shane Tomlinson <stomlinson@mozilla.com>
2011-08-19 09:50:57 -0700

    Starting to add more tests and documentation.

--- browserid/static/dialog/test/qunit/browserid-network_test.js
+++ browserid/static/dialog/test/qunit/browserid-network_test.js
@@ -1,9 +1,75 @@
-steal.plugins("funcunit/qunit").then("/dialog/resources/browserid-network", function() {
+/**
+ * This test assumes for authentication that there is a user named 
+ * "testuser@testuser.com" with the password "testuser"
+ */
+steal.plugins("jquery", "funcunit/qunit").then("/dialog/resources/browserid-network", function() {
   module("browserid-network");
   
   test("setOrigin", function() {
     BrowserIDNetwork.setOrigin("https://www.mozilla.com");
 
     equal("www.mozilla.com", BrowserIDNetwork.origin, "origin's are properly filtered");
+  });
+
+
+  test("authenticate with valid user", function() {
+    BrowserIDNetwork.authenticate("testuser@testuser.com", "testuser", function onSuccess(authenticated) {
+      start();
+      equal(true, authenticated, "valid authentication");
+    }, function onFailure() {
+      start();
+      ok(false, "valid authentication");
+    });
+
+    stop();
+  });
+
+  test("authenticate with invalid user", function() {
+    BrowserIDNetwork.authenticate("testuser@testuser.com", "invalid", function onSuccess(authenticated) {
+      start();
+      equal(false, authenticated, "invalid authentication");
+    }, function onFailure() {
+      start();
+      ok(false, "invalid authentication");
+    });
+
+    stop();
+  });
+
+  test("checkAuth", function() {
+    BrowserIDNetwork.authenticate("testuser@testuser.com", "testuser", function onSuccess(authenticated) {
+      BrowserIDNetwork.checkAuth(function onSuccess(authenticated) {
+        start();
+        equal(true, authenticated, "we have an authentication");
+      }, function onFailure() {
+        start();
+        ok(false, "checkAuth failure");
+      });
+    }, function onFailure() {
+      start();
+      ok(false, "valid authentication");
+    });
+
+    stop();
+  });
+
+  test("logout->checkAuth: are we really logged out?", function() {
+    BrowserIDNetwork.authenticate("testuser@testuser.com", "testuser", function onSuccess(authenticated) {
+      BrowserIDNetwork.logout(function onSuccess(authenticated) {
+        BrowserIDNetwork.checkAuth(function onSuccess(authenticated) {
+          start();
+          equal(false, authenticated, "after logout, we are not authenticated");
+        }, function onFailure() {
+          start();
+          ok(false, "checkAuth failure");
+        });
+      });
+    });
+
+    stop();
+  });
+
+  test("stageUser", function() {
+    ok(true, "yar");
   });
 });
