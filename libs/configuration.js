d787012a2d72963c3abb3aeae65142e20c615d5e
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-10-11 17:08:31 -0600

    allow code in the test environment to dynamically change system configuration to test stuff (like bcrypt work factor update, for instance)

--- libs/configuration.js
+++ libs/configuration.js
@@ -51,9 +51,18 @@ var g_config = {
 
 // get the value for a given key, this mechanism allows the rest of the
 // application to reach in and set
-exports.get = function(val) {
-  if (val === 'env') return process.env['NODE_ENV'];
-  return g_config[val];
+exports.get = function(key) {
+  if (key === 'env') return process.env['NODE_ENV'];
+  return g_config[key];
+}
+
+// allow the application to set configuration variables
+// iff we're in a test_ environment
+exports.set = function(key, val) {
+  if (!/^test_/.test(exports.get('env')))
+    throw "you may only set configuration variables in a test_ environment " +
+          "(not in '" + exports.get('env') + "')";
+  g_config[key] = val;
 }
 
 // *** the various deployment configurations ***
