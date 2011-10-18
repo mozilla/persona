051dc371bf0c82e84281ffeb63ad0242cf00290b
Shane Tomlinson <stomlinson@mozilla.com>
2011-10-18 09:14:05 +0100

    Starting the shell of a test for the page controller.

--- browserid/static/dialog/test/qunit/qunit.js
+++ browserid/static/dialog/test/qunit/qunit.js
@@ -3,8 +3,15 @@ steal("/dialog/resources/browserid.js",
       "/dialog/resources/tooltip.js",
       "/dialog/resources/validation.js",
       "/dialog/resources/underscore-min.js")
-  .plugins("funcunit/qunit")
+  .plugins(
+    "jquery", 
+    "jquery/controller",
+    "jquery/controller/subscribe",
+    "jquery/controller/view",
+    "jquery/view/ejs",
+    "funcunit/qunit")
   .then("browserid_unit_test")
+  .then("controllers/page_controller_unit_test")
   .then("resources/validation_unit_test")
   .then("resources/storage_unit_test")
   .then("resources/network_unit_test")
