29a24826e856b29b0025bd614092765662e1f52b
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-11-04 09:59:21 -0600

    remove CORS from repository.  In-repo example now properly forwards requests to verifier from server.  closes #245

--- scripts/serve_example.js
+++ scripts/serve_example.js
@@ -5,11 +5,12 @@ const
 express = require('express'),
 path = require('path'),
 urlparse = require('urlparse'),
-postprocess = require('postprocess');
+postprocess = require('postprocess'),
+querystring = require('querystring');
 
 var exampleServer = express.createServer();
 
-exampleServer.use(express.logger());
+exampleServer.use(express.logger({ format: 'dev' }));
 
 if (process.env['BROWSERID_URL']) {
   var burl = urlparse(process.env['BROWSERID_URL']).validate().normalize().originOnly().toString();
@@ -21,6 +22,56 @@ if (process.env['BROWSERID_URL']) {
 }
 
 exampleServer.use(express.static(path.join(__dirname, "..", "example")));
+
+exampleServer.use(express.bodyParser());
+
+exampleServer.post('/process_assertion', function(req, res, next) {
+  var verifier = urlparse(process.env['VERIFIER_URL']);
+  var meth = verifier.scheme === 'http' ? require('http') : require('https');
+
+  var vreq = meth.request({
+    host: verifier.host,
+    port: verifier.port,
+    path: verifier.path,
+    method: 'POST'
+  }, function(vres) {
+    var body = "";
+    vres.on('data', function(chunk) { body+=chunk; } )
+        .on('end', function() {
+          try {
+            console.log(body);
+            var verifierResp = JSON.parse(body);
+            var valid = verifierResp && verifierResp.status === "okay";
+            var email = valid ? verifierResp.email : null;
+            if (valid) {
+              console.log("assertion verified successfully for email:", email);
+            } else {
+              console.log("failed to verify assertion:", verifierResp.reason);
+            }
+            res.json(verifierResp);
+          } catch(e) {
+            console.log("non-JSON response from verifier");
+            // bogus response from verifier!  return null
+            res.json(null);
+          }
+        });
+  });
+  vreq.setHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  // An "audience" argument is embedded in the assertion and must match our hostname.
+  // Because this one server runs on multiple different domain names we just use
+  // the host parameter out of the request.
+  var audience = req.headers['host'] ? req.headers['host'] : localHostname;   
+  var data = querystring.stringify({
+    assertion: req.body.assertion,
+    audience: audience
+  });
+  vreq.setHeader('Content-Length', data.length);
+  vreq.write(data);
+  vreq.end();
+  console.log("verifying assertion!");
+
+});
 
 exampleServer.listen(
   process.env['PORT'] || 10001,
