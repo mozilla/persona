acb7dd46f92873c1ff666d9585404966b2f6af9b
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-11-18 17:04:37 -0700

    store public key on disk as a certificate which has embedded creation time.  use that as the value returned in /wsapi/session_context as domain_key_creation_time - now you can update keys at any time and outstanding client certs are expired automatically - closes #599

--- scripts/generate_ephemeral_keys.sh
+++ scripts/generate_ephemeral_keys.sh
@@ -10,14 +10,25 @@ if [ -f $VAR/root.publickey ] ; then
 fi
 
 GENERATE_KEYPAIR=`which generate-keypair 2> /dev/null`
+CERTIFY=`which certify 2> /dev/null`
 
 if [ ! -x "$GENERATE_KEYPAIR" ] ; then
     echo "can't find generate-keypair from the jwcrypto package.  try: npm install"
     exit 1
 fi
 
+if [ ! -x "$CERTIFY" ] ; then
+    echo "can't find certify from the jwcrypto package.  try: rm -rf node_modules && npm install"
+    exit 1
+fi
+
 echo '*** Generating ephemeral keys used for testing ***'
 $GENERATE_KEYPAIR -k 128 -a rsa
 mkdir -p $VAR
-mv key.publickey $VAR/root.publickey
+
+# public key will be stored as a self signed certificate with an embedded
+# creation date (so that if the key is updated, we can revoke outstanding
+# certificates - GH-599 & GH-600)
+$CERTIFY -s key.secretkey -p key.publickey > $VAR/root.cert
+rm key.publickey
 mv key.secretkey $VAR/root.secretkey
