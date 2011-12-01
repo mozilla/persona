1df394fc5c2a81f5c2299209d89678fc7577d40d
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-12-01 10:15:35 -0700

    update to latest jschannel - closes #534 (innocuous console errors when messages are posted to the frame where include.js is loaded that do not have valid JSON as a payload)

--- resources/static/lib/jschannel.js
+++ resources/static/lib/jschannel.js
@@ -1,4 +1,3 @@
-/*global console: true, postMessage: true */
 /**
  * js_channel is a very lightweight abstraction on top of
  * postMessage which defines message formats and semantics
@@ -37,15 +36,17 @@
  *    + (optional) any params
  */
 
-;Channel = (function() {
+;var Channel = (function() {
+    "use strict";
+
     // current transaction id, start out at a random *odd* number between 1 and a million
     // There is one current transaction counter id per page, and it's shared between
     // channel instances.  That means of all messages posted from a single javascript
     // evaluation context, we'll never have two with the same id.
     var s_curTranId = Math.floor(Math.random()*1000001);
 
-    // no two bound channels in the same javascript evaluation context may have the same origin & scope.
-    // futher if two bound channels have the same scope, they may not have *overlapping* origins
+    // no two bound channels in the same javascript evaluation context may have the same origin, scope, and window.
+    // futher if two bound channels have the same window and scope, they may not have *overlapping* origins
     // (either one or both support '*').  This restriction allows a single onMessage handler to efficiently
     // route messages based on origin and scope.  The s_boundChans maps origins to scopes, to message
     // handlers.  Request and Notification messages are routed using this table.
@@ -53,42 +54,60 @@
     var s_boundChans = { };
 
     // add a channel to s_boundChans, throwing if a dup exists
-    function s_addBoundChan(origin, scope, handler) {
+    function s_addBoundChan(win, origin, scope, handler) {
+        function hasWin(arr) {
+            for (var i = 0; i < arr.length; i++) if (arr[i].win === win) return true;
+            return false;
+        }
+
         // does she exist?
         var exists = false;
+
+
         if (origin === '*') {
             // we must check all other origins, sadly.
             for (var k in s_boundChans) {
                 if (!s_boundChans.hasOwnProperty(k)) continue;
                 if (k === '*') continue;
                 if (typeof s_boundChans[k][scope] === 'object') {
-                    exists = true;
+                    exists = hasWin(s_boundChans[k][scope]);
+                    if (exists) break;
                 }
             }
         } else {
             // we must check only '*'
-            if ((s_boundChans['*'] && s_boundChans['*'][scope]) ||
-                (s_boundChans[origin] && s_boundChans[origin][scope]))
+            if ((s_boundChans['*'] && s_boundChans['*'][scope])) {
+                exists = hasWin(s_boundChans['*'][scope]);
+            }
+            if (!exists && s_boundChans[origin] && s_boundChans[origin][scope])
             {
-                exists = true;
+                exists = hasWin(s_boundChans[origin][scope]);
             }
         }
-        if (exists) throw "A channel already exists which overlaps with origin '"+ origin +"' and has scope '"+scope+"'";
+        if (exists) throw "A channel is already bound to the same window which overlaps with origin '"+ origin +"' and has scope '"+scope+"'";
 
         if (typeof s_boundChans[origin] != 'object') s_boundChans[origin] = { };
-        s_boundChans[origin][scope] = handler;
+        if (typeof s_boundChans[origin][scope] != 'object') s_boundChans[origin][scope] = [ ];
+        s_boundChans[origin][scope].push({win: win, handler: handler});
     }
 
-    function s_removeBoundChan(origin, scope) {
-        delete s_boundChans[origin][scope];
-        // possibly leave a empty object around.  whatevs.
+    function s_removeBoundChan(win, origin, scope) {
+        var arr = s_boundChans[origin][scope];
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].win === win) {
+                arr.splice(i,1);
+            }
+        }
+        if (s_boundChans[origin][scope].length === 0) {
+            delete s_boundChans[origin][scope]
+        }
     }
 
     function s_isArray(obj) {
-      if (Array.isArray) return Array.isArray(obj);
-      else {
-        return (obj.constructor.toString().indexOf("Array") != -1);
-      }
+        if (Array.isArray) return Array.isArray(obj);
+        else {
+            return (obj.constructor.toString().indexOf("Array") != -1);
+        }
     }
 
     // No two outstanding outbound messages may have the same id, period.  Given that, a single table
@@ -102,13 +121,17 @@
     // arrangement allows certain efficiencies, message data is only parsed once and dispatch
     // is more efficient, especially for large numbers of simultaneous channels.
     var s_onMessage = function(e) {
-        var m = JSON.parse(e.data);
-        if (typeof m !== 'object') return;
+        try {
+          var m = JSON.parse(e.data);
+          if (typeof m !== 'object' || m === null) throw "malformed";
+        } catch(e) {
+          // just ignore any posted messages that do not consist of valid JSON
+          return;
+        }
 
+        var w = e.source;
         var o = e.origin;
-        var s = null;
-        var i = null;
-        var meth = null;
+        var s, i, meth;
 
         if (typeof m.method === 'string') {
             var ar = m.method.split('::');
@@ -122,20 +145,35 @@
 
         if (typeof m.id !== 'undefined') i = m.id;
 
+        // w is message source window
         // o is message origin
         // m is parsed message
         // s is message scope
-        // i is message id (or null)
+        // i is message id (or undefined)
         // meth is unscoped method name
         // ^^ based on these factors we can route the message
 
         // if it has a method it's either a notification or a request,
         // route using s_boundChans
         if (typeof meth === 'string') {
+            var delivered = false;
             if (s_boundChans[o] && s_boundChans[o][s]) {
-                s_boundChans[o][s](o, meth, m);
-            } else if (s_boundChans['*'] && s_boundChans['*'][s]) {
-                s_boundChans['*'][s](o, meth, m);
+                for (var i = 0; i < s_boundChans[o][s].length; i++) {
+                    if (s_boundChans[o][s][i].win === w) {
+                        s_boundChans[o][s][i].handler(o, meth, m);
+                        delivered = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!delivered && s_boundChans['*'] && s_boundChans['*'][s]) {
+                for (var i = 0; i < s_boundChans['*'][s].length; i++) {
+                    if (s_boundChans['*'][s][i].win === w) {
+                        s_boundChans['*'][s][i].handler(o, meth, m);
+                        break;
+                    }
+                }
             }
         }
         // otherwise it must have an id (or be poorly formed
@@ -154,7 +192,7 @@
      *
      * Arguments to Channel.build(cfg):
      *
-     *   cfg.window - the remote window with which we'll communication
+     *   cfg.window - the remote window with which we'll communicate
      *   cfg.origin - the expected origin of the remote window, may be '*'
      *                which matches any origin
      *   cfg.scope  - the 'scope' of messages.  a scope string that is
@@ -188,7 +226,7 @@
                     try { if (typeof m !== 'string') m = JSON.stringify(m); } catch(e) { }
                     console.log("["+chanId+"] " + m);
                 }
-            };
+            }
 
             /* browser capabilities check */
             if (!window.postMessage) throw("jschannel cannot run this browser, no postMessage");
@@ -213,7 +251,7 @@
                 if (cfg.origin === "*") validOrigin = true;
                 // allow valid domains under http and https.  Also, trim paths off otherwise valid origins.
                 else if (null !== (oMatch = cfg.origin.match(/^https?:\/\/(?:[-a-zA-Z0-9\.])+(?::\d+)?/))) {
-                    cfg.origin = oMatch[0];
+                    cfg.origin = oMatch[0].toLowerCase();
                     validOrigin = true;
                 }
             }
@@ -222,7 +260,7 @@
 
             if (typeof cfg.scope !== 'undefined') {
                 if (typeof cfg.scope !== 'string') throw 'scope, when specified, must be a string';
-                if (cfg.scope.split('::').length > 1) throw "scope may not contain double colons: '::'";
+                if (cfg.scope.split('::').length > 1) throw "scope may not contain double colons: '::'"
             }
 
             /* private variables */
@@ -252,7 +290,7 @@
                     origin: origin,
                     invoke: function(cbName, v) {
                         // verify in table
-                        if (!inTbl[id]) throw "attempting to invoke a callback of a non-existant transaction: " + id;
+                        if (!inTbl[id]) throw "attempting to invoke a callback of a nonexistent transaction: " + id;
                         // verify that the callback name is valid
                         var valid = false;
                         for (var i = 0; i < callbacks.length; i++) if (cbName === callbacks[i]) { valid = true; break; }
@@ -264,7 +302,7 @@
                     error: function(error, message) {
                         completed = true;
                         // verify in table
-                        if (!inTbl[id]) throw "error called for non-existant message: " + id;
+                        if (!inTbl[id]) throw "error called for nonexistent message: " + id;
 
                         // remove transaction from table
                         delete inTbl[id];
@@ -275,7 +313,7 @@
                     complete: function(v) {
                         completed = true;
                         // verify in table
-                        if (!inTbl[id]) throw "complete called for non-existant message: " + id;
+                        if (!inTbl[id]) throw "complete called for nonexistent message: " + id;
                         // remove transaction from table
                         delete inTbl[id];
                         // send complete
@@ -291,8 +329,20 @@
                         return completed;
                     }
                 };
-            };
+            }
 
+            var setTransactionTimeout = function(transId, timeout, method) {
+              return window.setTimeout(function() {
+                if (outTbl[transId]) {
+                  // XXX: what if client code raises an exception here?
+                  var msg = "timeout (" + timeout + "ms) exceeded on method '" + method + "'";
+                  (1,outTbl[transId].error)("timeout_error", msg);
+                  delete outTbl[transId];
+                  delete s_transIds[transId];
+                }
+              }, timeout);
+            }
+            
             var onMessage = function(origin, method, m) {
                 // if an observer was specified at allocation time, invoke it
                 if (typeof cfg.gotMessageObserver === 'function') {
@@ -331,7 +381,7 @@
                                         return function(params) {
                                             return trans.invoke(cbName, params);
                                         }
-                                    }());
+                                    })();
                                 }
                             }
                             var resp = regTbl[method](trans, m.params);
@@ -340,17 +390,17 @@
                             // automagic handling of exceptions:
                             var error = "runtime_error";
                             var message = null;
-                            // * if its a string then it gets an error code of 'runtime_error' and string is the message
+                            // * if it's a string then it gets an error code of 'runtime_error' and string is the message
                             if (typeof e === 'string') {
                                 message = e;
                             } else if (typeof e === 'object') {
                                 // either an array or an object
-                                // * if its an array of length two, then  array[0] is the code, array[1] is the error message
+                                // * if it's an array of length two, then  array[0] is the code, array[1] is the error message
                                 if (e && s_isArray(e) && e.length == 2) {
                                     error = e[0];
                                     message = e[1];
                                 }
-                                // * if its an object then we'll look form error and message parameters
+                                // * if it's an object then we'll look form error and message parameters
                                 else if (typeof e.error === 'string') {
                                     error = e.error;
                                     if (!e.message) message = "";
@@ -363,6 +413,10 @@
                             if (message === null) {
                                 try {
                                     message = JSON.stringify(e);
+                                    /* On MSIE8, this can result in 'out of memory', which
+                                     * leaves message undefined. */
+                                    if (typeof(message) == 'undefined')
+                                      message = e.toString();
                                 } catch (e2) {
                                     message = e.toString();
                                 }
@@ -387,8 +441,8 @@
                         if (m.error) {
                             (1,outTbl[m.id].error)(m.error, m.message);
                         } else {
-                          if (m.result !== undefined) (1,outTbl[m.id].success)(m.result);
-                          else (1,outTbl[m.id].success)();
+                            if (m.result !== undefined) (1,outTbl[m.id].success)(m.result);
+                            else (1,outTbl[m.id].success)();
                         }
                         delete outTbl[m.id];
                         delete s_transIds[m.id];
@@ -406,7 +460,7 @@
             }
 
             // now register our bound channel for msg routing
-            s_addBoundChan(cfg.origin, ((typeof cfg.scope === 'string') ? cfg.scope : ''), onMessage);
+            s_addBoundChan(cfg.window, cfg.origin, ((typeof cfg.scope === 'string') ? cfg.scope : ''), onMessage);
 
             // scope method names based on cfg.scope specified when the Channel was instantiated
             var scopeMethod = function(m) {
@@ -479,6 +533,7 @@
 
                     if (regTbl[method]) throw "method '"+method+"' is already bound!";
                     regTbl[method] = cb;
+                    return this;
                 },
                 call: function(m) {
                     if (!m) throw 'missing arguments to call function';
@@ -511,6 +566,12 @@
                     var msg = { id: s_curTranId, method: scopeMethod(m.method), params: m.params };
                     if (callbackNames.length) msg.callbacks = callbackNames;
 
+                    if (m.timeout)
+                      // XXX: This function returns a timeout ID, but we don't do anything with it.
+                      // We might want to keep track of it so we can cancel it using clearTimeout()
+                      // when the transaction completes.
+                      setTransactionTimeout(s_curTranId, m.timeout, scopeMethod(m.method));
+
                     // insert into the transaction table
                     outTbl[s_curTranId] = { callbacks: callbacks, error: m.error, success: m.success };
                     s_transIds[s_curTranId] = onMessage;
@@ -528,7 +589,7 @@
                     postMessage({ method: scopeMethod(m.method), params: m.params });
                 },
                 destroy: function () {
-                    s_removeBoundChan(cfg.origin, ((typeof cfg.scope === 'string') ? cfg.scope : ''));
+                    s_removeBoundChan(cfg.window, cfg.origin, ((typeof cfg.scope === 'string') ? cfg.scope : ''));
                     if (window.removeEventListener) window.removeEventListener('message', onMessage, false);
                     else if(window.detachEvent) window.detachEvent('onmessage', onMessage);
                     ready = false;
