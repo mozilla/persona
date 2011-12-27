mozilla/persona

commit f9ab1ba9c2cab2aede58e20d613aee585e1167f5

Starting to update the dialog controller to handle code versioning.
* Adding getCodeVersion to network.
* Adding error messages for if session_context fails during getCodeVersion.
* Adding a code check module.
* Updating start so that most modules are only registered whenever code is up to date.
* If the scripts are out of date, append a new script to the head.
* Cleaning up the dialog module's interface with the channel.
* Updating the compression script.
* Splitting off the actions into its own page module/controller for easier testing.

issue #226
issue #687

resources/static/dialog/controllers/actions.js
@@ -1,0 +1,166 @@
+/*jshint brgwser:true, jQuery: true, forin: true, laxbreak:true */
+/*global _: true, BrowserID: true, PageController: true */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla BrowserID.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+BrowserID.Modules.Actions = (function() {
+  "use strict";
+
+  var bid = BrowserID,
+      sc,
+      serviceManager = bid.module,
+      user = bid.User,
+      errors = bid.Errors,
+      runningService,
+      onsuccess,
+      onerror;
+
+  function startService(name, options) {
+    // Only one service outside of the main dialog allowed.
+    if(runningService) {
+      serviceManager.stop(runningService);
+    }
+    runningService = name;
+    return serviceManager.start(name, options);
+  }
+
+  function startRegCheckService(email, verifier, message) {
+    this.confirmEmail = email;
+
+    var controller = startService("check_registration", {
+      email: email,
+      verifier: verifier,
+      verificationMessage: message
+    });
+    controller.startCheck();
+  }
+
+  var Module = bid.Modules.PageModule.extend({
+    start: function(data) {
+      var self=this;
+
+      data = data || {};
+
+      onsuccess = data.onsuccess;
+      onerror = data.onerror;
+
+      sc.start.call(self, data);
+
+      if(data.ready) {
+        data.ready();
+      }
+    },
+
+    doOffline: function() {
+      this.renderError("offline", {});
+    },
+
+    doCancel: function() {
+      onsuccess && onsuccess(null);
+    },
+
+    doConfirmUser: function(email) {
+      startRegCheckService.call(this, email, "waitForUserValidation", "user_confirmed");
+    },
+
+    doPickEmail: function(info) {
+      startService("pick_email", info);
+    },
+
+    doAddEmail: function() {
+      startService("add_email", {});
+    },
+
+    doAuthenticate: function(info) {
+      startService("authenticate", info);
+    },
+
+    doAuthenticateWithRequiredEmail: function(info) {
+      startService("required_email", info);
+    },
+
+    doForgotPassword: function(email) {
+      startService("forgot_password", {
+        email: email
+      });
+    },
+
+    doConfirmEmail: function(email) {
+      startRegCheckService.call(this, email, "waitForEmailValidation", "email_confirmed");
+    },
+
+    doEmailConfirmed: function() {
+      var self=this;
+      // yay!  now we need to produce an assertion.
+      user.getAssertion(self.confirmEmail, function(assertion) {
+        self.publish("assertion_generated", {
+          assertion: assertion
+        });
+      }, self.getErrorDialog(errors.getAssertion));
+    },
+
+    doAssertionGenerated: function(assertion) {
+      // Clear onerror before the call to onsuccess - the code to onsuccess
+      // calls window.close, which would trigger the onerror callback if we
+      // tried this afterwards.
+      onerror = null;
+      onsuccess && onsuccess(assertion);
+    },
+
+    doNotMe: function() {
+      var self=this;
+      user.logoutUser(self.publish.bind(self, "logged_out"), self.getErrorDialog(errors.logoutUser));
+    },
+
+    doSyncThenPickEmail: function() {
+      var self = this;
+      user.syncEmails(self.doPickEmail.bind(self),
+        self.getErrorDialog(errors.syncEmails));
+    },
+
+    doCheckAuth: function() {
+      var self=this;
+      user.checkAuthenticationAndSync(function onSuccess() {}, function onComplete(authenticated) {
+        self.publish("authentication_checked", {
+          authenticated: authenticated
+        });
+      }, self.getErrorDialog(errors.checkAuthentication));
+    }
+
+  });
+
+  sc = Module.sc;
+
+  return Module;
+}());

resources/static/dialog/controllers/dialog.js
@@ -1,5 +1,5 @@
 /*jshint browser:true, jQuery: true, forin: true, laxbreak:true */
-/*global setupChannel:true, BrowserID: true */
+/*global BrowserID: true */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -42,56 +42,41 @@ BrowserID.Modules.Dialog = (function() {
   var bid = BrowserID,
       user = bid.User,
       errors = bid.Errors,
+      channel = bid.Channel,
       dom = bid.DOM,
-      offline = false,
-      win = window,
-      serviceManager = bid.module,
-      runningService;
-
-  function startService(name, options) {
-    // Only one service outside of the main dialog allowed.
-    if(runningService) {
-      serviceManager.stop(runningService);
-    }
-    runningService = name;
-    return serviceManager.start(name, options);
-  }
-
-  function startRegCheckService(email, verifier, message) {
-    this.confirmEmail = email;
-
-    var controller = startService("check_registration", {
-      email: email,
-      verifier: verifier,
-      verificationMessage: message
-    });
-    controller.startCheck();
-  }
+      win = window;
 
   function checkOnline() {
     if ('onLine' in navigator && !navigator.onLine) {
-      this.doOffline();
+      this.publish("offline");
       return false;
     }
 
     return true;
   }
 
-  function onWinUnload() {
-    // do this only if something else hasn't declared success
-    var self=this;
-    if (!self.success) {
-      bid.Storage.setStagedOnBehalfOf("");
-      self.doCancel();
-    }
-    window.teardownChannel();
+  function startActions(onsuccess, onerror) {
+    var actions = BrowserID.Modules.Actions.create();
+    actions.start({
+      onsuccess: onsuccess,
+      onerror: onerror
+    });
+    return actions;
   }
 
-  function setupChannel() {
+  function startStateMachine(controller) {
+    // start this directly because it should always be running.
+    var machine = BrowserID.StateMachine.create();
+    machine.start({
+      controller: controller
+    });
+  }
+
+  function startChannel() {
     var self = this;
 
     try {
-      win.setupChannel(self);
+      channel.open(self);
     } catch (e) {
       self.renderError("error", {
         action: errors.relaySetup
@@ -104,161 +89,53 @@ BrowserID.Modules.Dialog = (function() {
     dom.setInner("#sitename", user.getHostname());
   }
 
+  function onWindowUnload() {
+    this.publish("window_unload");
+    channel.close();
+  }
+
   var Dialog = bid.Modules.PageModule.extend({
-      init: function(options) {
-        offline = false;
+    init: function(options) {
+      var self=this;
 
-        options = options || {};
+      options = options || {};
 
-        if (options.window) {
-          win = options.window;
-        }
+      win = options.window || window;
 
-        var self=this;
+      Dialog.sc.init.call(self, options);
 
-        Dialog.sc.init.call(self, options);
+      startChannel.call(self);
 
-        // keep track of where we are and what we do on success and error
-        self.onsuccess = null;
-        self.onerror = null;
-
-        // start this directly because it should always be running.
-        var machine = BrowserID.StateMachine.create();
-        machine.start({
-          controller: this
-        });
-
-        setupChannel.call(self);
-      },
-
-      getVerifiedEmail: function(origin_url, onsuccess, onerror) {
-        return this.get(origin_url, {}, onsuccess, onerror);
-      },
-
-      get: function(origin_url, params, onsuccess, onerror) {
-        var self=this;
-
-        if(checkOnline.call(self)) {
-          self.onsuccess = onsuccess;
-          self.onerror = onerror;
-
-          params = params || {};
-
-          self.allowPersistent = !!params.allowPersistent;
-          self.requiredEmail = params.requiredEmail;
-
-          setOrigin(origin_url);
-
-          self.bind(win, "unload", onWinUnload);
-
-          self.doCheckAuth();
-        }
-      },
-
-      doOffline: function() {
-        this.renderError("offline", {});
-        offline = true;
-      },
-
-      doXHRError: function(info) {
-        if (!offline) {
-          this.renderError("error", $.extend({
-            action: errors.xhrError
-          }, info));
-        }
-      },
-
-      doConfirmUser: function(email) {
-        startRegCheckService.call(this, email, "waitForUserValidation", "user_confirmed");
-      },
-
-      doCancel: function() {
-        var self=this;
-        if (self.onsuccess) {
-          self.onsuccess(null);
-        }
-      },
-
-      doPickEmail: function() {
-        var self=this;
-        startService("pick_email", {
-          // XXX ideal is to get rid of this and have a User function
-          // that takes care of getting email addresses AND the last used email
-          // for this site.
-          origin: user.getHostname(),
-          allow_persistent: self.allowPersistent
-        });
-      },
-
-      doAddEmail: function() {
-        startService("add_email", {});
-      },
-
-      doAuthenticate: function(info) {
-        startService("authenticate", info);
-      },
-
-      doAuthenticateWithRequiredEmail: function(info) {
-        startService("required_email", info);
-      },
-
-      doForgotPassword: function(email) {
-        startService("forgot_password", {
-          email: email
-        });
-      },
-
-      doConfirmEmail: function(email) {
-        startRegCheckService.call(this, email, "waitForEmailValidation", "email_confirmed");
-      },
-
-      doEmailConfirmed: function() {
-        var self=this;
-        // yay!  now we need to produce an assertion.
-        user.getAssertion(self.confirmEmail, self.doAssertionGenerated.bind(self),
-          self.getErrorDialog(errors.getAssertion));
-      },
-
-      doAssertionGenerated: function(assertion) {
-        var self=this;
-        // Clear onerror before the call to onsuccess - the code to onsuccess
-        // calls window.close, which would trigger the onerror callback if we
-        // tried this afterwards.
-        self.onerror = null;
-        self.success = true;
-        self.onsuccess(assertion);
-      },
-
-      doNotMe: function() {
-        var self=this;
-        user.logoutUser(self.publish.bind(self, "auth"), self.getErrorDialog(errors.logoutUser));
-      },
-
-      doSyncThenPickEmail: function() {
-        var self = this;
-        user.syncEmails(self.doPickEmail.bind(self),
-          self.getErrorDialog(errors.signIn));
-      },
-
-      doCheckAuth: function() {
-        var self=this;
-        user.checkAuthenticationAndSync(function onSuccess() {},
-          function onComplete(authenticated) {
-            if (self.requiredEmail) {
-              self.publish("authenticate_with_required_email", {
-                email: self.requiredEmail,
-                authenticated: authenticated
-              });
-            }
-            else if (authenticated) {
-              self.publish("pick_email");
-            } else {
-              self.publish("auth");
-            }
-          }, self.getErrorDialog(errors.checkAuthentication));
+      options.ready && _.defer(options.ready);
     },
 
-    doWinUnload: onWinUnload
+    getVerifiedEmail: function(origin_url, success, error) {
+      return this.get(origin_url, {}, success, error);
+    },
+
+    get: function(origin_url, params, success, error) {
+      var self=this;
+
+      setOrigin(origin_url);
+
+      var actions = startActions.call(self, success, error);
+      startStateMachine.call(self, actions);
+
+      if(checkOnline.call(self)) {
+        params = params || {};
+
+        params.hostname = user.getHostname();
+
+        self.bind(win, "unload", onWindowUnload);
+
+        self.publish("start", params);
+      }
+    }
+
+    // BEGIN TESTING API
+    ,
+    onWindowUnload: onWindowUnload
+    // END TESTING API
 
   });
 

resources/static/dialog/resources/state_machine.js
@@ -36,10 +36,10 @@
  * ***** END LICENSE BLOCK ***** */
 (function() {
   var bid = BrowserID,
-      user = bid.User,
       mediator = bid.Mediator,
       subscriptions = [],
-      stateStack = [];
+      stateStack = [],
+      controller;
 
   function subscribe(message, cb) {
     subscriptions.push(mediator.subscribe(message, cb));
@@ -52,8 +52,7 @@
   }
 
   function pushState(funcName) {
-    var args = [].slice.call(arguments, 1),
-        controller = this.controller;
+    var args = [].slice.call(arguments, 1);
 
     // Remember the state and the information for the state in case we have to
     // go back to it.
@@ -75,14 +74,12 @@
     var gotoState = stateStack[stateStack.length - 1];
 
     if(gotoState) {
-      var controller = this.controller;
       controller[gotoState.funcName].apply(controller, gotoState.args);
     }
   }
 
   function startStateMachine() {
     var self = this,
-        controller = self.controller,
         gotoState = pushState.bind(self),
         cancelState = popState.bind(self);
 
@@ -90,8 +87,50 @@
       gotoState("doOffline");
     });
 
-    subscribe("cancel_state", function(msg, info) {
-      cancelState();
+    subscribe("start", function(msg, info) {
+      info = info || {};
+
+      self.hostname = info.hostname;
+      self.allowPersistent = !!info.allowPersistent;
+      self.requiredEmail = info.requiredEmail;
+
+      gotoState("doCheckAuth");
+    });
+
+    subscribe("cancel", function() {
+      gotoState("doCancel");
+    });
+
+    subscribe("window_unload", function() {
+      if(!self.success) {
+        bid.Storage.setStagedOnBehalfOf("");
+        gotoState("doCancel");
+      }
+    });
+
+    subscribe("authentication_checked", function(msg, info) {
+      var authenticated = info.authenticated;
+
+      if (self.requiredEmail) {
+        // XXX get this out of here and into the state machine!
+        gotoState("doAuthenticateWithRequiredEmail", {
+          email: self.requiredEmail,
+          authenticated: authenticated
+        });
+      }
+      else if (authenticated) {
+        mediator.publish("pick_email");
+      } else {
+        mediator.publish("authenticate");
+      }
+    });
+
+    subscribe("authenticate", function(msg, info) {
+      info = info || {};
+
+      gotoState("doAuthenticate", {
+        email: info.email
+      });
     });
 
     subscribe("user_staged", function(msg, info) {
@@ -107,7 +146,18 @@
     });
 
     subscribe("pick_email", function() {
-      gotoState("doPickEmail");
+      gotoState("doPickEmail", {
+        origin: self.hostname,
+        allow_persistent: self.allowPersistent
+      });
+    });
+
+    subscribe("notme", function() {
+      gotoState("doNotMe");
+    });
+
+    subscribe("logged_out", function() {
+      mediator.publish("authenticate");
     });
 
     subscribe("authenticated", function(msg, info) {
@@ -123,6 +173,7 @@
     });
 
     subscribe("assertion_generated", function(msg, info) {
+      self.success = true;
       if (info.assertion !== null) {
         gotoState("doAssertionGenerated", info.assertion);
       }
@@ -143,25 +194,10 @@
       gotoState("doEmailConfirmed");
     });
 
-    subscribe("notme", function() {
-      gotoState("doNotMe");
+    subscribe("cancel_state", function(msg, info) {
+      cancelState();
     });
 
-    subscribe("auth", function(msg, info) {
-      info = info || {};
-
-      gotoState("doAuthenticate", {
-        email: info.email
-      });
-    });
-
-    subscribe("start", function() {
-      gotoState("doCheckAuth");
-    });
-
-    subscribe("cancel", function() {
-      gotoState("doCancel");
-    });
   }
 
   var StateMachine = BrowserID.Class({
@@ -171,7 +207,12 @@
 
     start: function(options) {
       options = options || {};
-      this.controller = options.controller;
+
+      controller = options.controller;
+      if(!controller) {
+        throw "start: controller must be specified";
+      }
+
       startStateMachine.call(this);
     },
 

resources/static/dialog/controllers/checkregistration.js
@@ -57,23 +57,25 @@ BrowserID.Modules.CheckRegistration = (function() {
       CheckRegistration.sc.start.call(self, options);
     },
 
-    startCheck: function() {
+    startCheck: function(oncomplete) {
       var self=this;
       user[self.verifier](self.email, function(status) {
         if (status === "complete") {
           user.syncEmails(function() {
             self.close(self.verificationMessage);
+            oncomplete && oncomplete();
           });
         }
         else if (status === "mustAuth") {
           self.close("auth", { email: self.email });
+          oncomplete && oncomplete();
         }
-      }, self.getErrorDialog(errors.registration));
+      }, self.getErrorDialog(errors.registration, oncomplete));
     },
 
     cancel: function() {
       var self=this;
-      // XXX this should change to cancelEmailValidation for email, but this 
+      // XXX this should change to cancelEmailValidation for email, but this
       // will work.
       user.cancelUserValidation();
       self.close("cancel_state");

resources/static/dialog/controllers/code_check.js
@@ -1,5 +1,5 @@
-/*jshint brgwser:true, jQuery: true, forin: true, laxbreak:true */
-/*global _: true, BrowserID: true, PageController: true */
+/*jshint browser:true, jQuery: true, forin: true, laxbreak:true */
+/*global BrowserID: true */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -34,56 +34,76 @@
  * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
-BrowserID.Modules.AddEmail = (function() {
+
+
+BrowserID.Modules.CodeCheck = (function() {
   "use strict";
 
   var bid = BrowserID,
-      helpers = bid.Helpers,
-      dialogHelpers = helpers.Dialog,
-      errors = bid.Errors,
-      tooltip = bid.Tooltip;
+      dom = bid.DOM,
+      sc,
+      expectedCodeVer;
 
-  function cancelEvent(callback) {
-    return function(event) {
-      event && event.preventDefault();
-      callback && callback();
-    }
+  function getMostRecentCodeVersion(oncomplete) {
+    bid.Network.codeVersion(oncomplete, this.getErrorDialog(bid.Errors.checkScriptVersion, oncomplete));
   }
 
-  function addEmail(callback) {
-    var email = helpers.getAndValidateEmail("#newEmail"),
-        self=this;
+  function updateCodeIfNeeded(oncomplete, version) {
+    var mostRecent = version === expectedCodeVer;
 
-    if (email) {
-      dialogHelpers.addEmail.call(self, email, callback);
+    function ready() {
+      oncomplete && oncomplete(mostRecent);
+    }
+
+    if(mostRecent) {
+      ready();
     }
     else {
-      callback && callback();
+      loadScript(version, ready);
     }
   }
 
+  function loadScript(version, oncomplete) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://browserid.org/dialog/production_v" + version + ".js";
+    document.head.appendChild(script);
 
-  function cancelAddEmail() {
-    this.close("cancel_state");
+    oncomplete();
   }
 
-  var AddEmail = bid.Modules.PageModule.extend({
-    start: function(options) {
-      var self=this;
+  var Module = bid.Modules.PageModule.extend({
+      start: function(data) {
+        var self=this;
 
-      self.renderDialog("addemail");
+        function complete(val) {
+          data.ready && data.ready(val);
+        }
 
-      self.bind("#cancelNewEmail", "click", cancelEvent(cancelAddEmail));
-      AddEmail.sc.start.call(self, options);
-    },
-    submit: addEmail
-    // BEGIN TESTING API
-    ,
-    addEmail: addEmail,
-    cancelAddEmail: cancelAddEmail
-    // END TESTING API
+        data = data || {};
+
+        if (data.code_ver) {
+          expectedCodeVer = data.code_ver;
+        }
+        else {
+          throw "init: code_ver must be defined";
+        }
+
+        getMostRecentCodeVersion.call(self, function(version) {
+          if(version) {
+            updateCodeIfNeeded.call(self, complete, version);
+          }
+          else {
+            complete();
+          }
+        });
+        sc.start.call(self, data);
+      }
   });
 
-  return AddEmail;
+  sc = Module.sc;
+
+  return Module;
 
 }());
+

resources/static/dialog/controllers/page.js
@@ -124,18 +124,12 @@ BrowserID.Modules.PageModule = (function() {
       screens.wait.show(body, body_vars);
     },
 
-    renderError: function(body, body_vars) {
+    renderError: function(body, body_vars, oncomplete) {
       screens.error.show(body, body_vars);
 
-      /**
-       * TODO XXX - Use the error-display for this.
-       */
-      this.bind("#openMoreInfo", "click", function(event) {
-        event.preventDefault();
+      bid.ErrorDisplay.start();
 
-        $("#moreInfo").slideDown();
-        $("#openMoreInfo").css({visibility: "hidden"});
-      });
+      $("#error").stop().css('opacity', 1).hide().fadeIn(ANIMATION_TIME, oncomplete);
     },
 
     validate: function() {
@@ -160,14 +154,16 @@ BrowserID.Modules.PageModule = (function() {
      * Get a curried function to an error dialog.
      * @method getErrorDialog
      * @method {object} action - info to use for the error dialog.  Should have
+     * @method {function} [onerror] - callback to call after the
+     * error has been displayed.
      * two fields, message, description.
      */
-    getErrorDialog: function(action) {
+    getErrorDialog: function(action, onerror) {
       var self=this;
       return function(lowLevelInfo) {
         self.renderError("error", $.extend({
           action: action
-        }, lowLevelInfo));
+        }, lowLevelInfo), onerror);
       }
     }
   });

resources/static/dialog/controllers/required_email.js
@@ -139,7 +139,7 @@ BrowserID.Modules.RequiredEmail = (function() {
           // verify ownership of the address.
           showTemplate(registered, registered);
           ready();
-        }, self.getErrorDialog(errors.isEmailRegistered));
+        }, self.getErrorDialog(errors.isEmailRegistered, ready));
       }
 
       function showTemplate(requireSignin, showPassword) {

resources/static/dialog/resources/channel.js
@@ -143,29 +143,27 @@
   }
 
 
-  if(window.BrowserID) {
-    BrowserID.Channel = {
-      /**
-       * Used to intialize the channel, mostly for unit testing to override
-       * window and navigator.
-       * @method init
-       */
-      init: init,
+  BrowserID.Channel = {
+    /**
+     * Used to intialize the channel, mostly for unit testing to override
+     * window and navigator.
+     * @method init
+     */
+    init: init,
 
-      /**
-       * Open the channel.
-       * @method open
-       * @param {object} options - contains:
-       * *   options.getVerifiedEmail {function} - function to /get
-       */
-      open: open,
+    /**
+     * Open the channel.
+     * @method open
+     * @param {object} options - contains:
+     * *   options.getVerifiedEmail {function} - function to /get
+     */
+    open: open,
 
-      /**
-       * Close the channel
-       */
-      close: close
-    };
-  }
+    /**
+     * Close the channel
+     */
+    close: close
+  };
 
   /**
    * This is here as a legacy API for addons/etc that are depending on

commit 6f54801f171088c9374ec5f7a539d8cde8765dfa

fix issue where when allowPersistent is true, and the user is not authenticated to browserid, they wouldn't be shown the 'keep me signed in' checkbox - closes #776 (presumably a regression introduce in f9ab1ba9c2cab2aede58e20d613aee585e1167f5 ?)

resources/static/dialog/controllers/actions.js
@@ -155,9 +155,9 @@ BrowserID.Modules.Actions = (function() {
       user.logoutUser(self.publish.bind(self, "logged_out"), self.getErrorDialog(errors.logoutUser));
     },
 
-    doSyncThenPickEmail: function() {
+    doSyncThenPickEmail: function(options) {
       var self = this;
-      user.syncEmails(self.doPickEmail.bind(self),
+      user.syncEmails(self.doPickEmail.bind(self, options),
         self.getErrorDialog(errors.syncEmails));
     },
 

resources/static/dialog/controllers/dialog.js
@@ -87,7 +87,6 @@ BrowserID.Modules.Dialog = (function() {
     }      
 
     try {
-      //
       WinChan.onOpen(function(origin, args, cb) {
         self.get(origin, args.params, function(r) {
           cb(r);

resources/static/dialog/resources/state_machine.js
@@ -167,7 +167,10 @@
     });
 
     subscribe("authenticated", function(msg, info) {
-      gotoState("doSyncThenPickEmail");
+      gotoState("doSyncThenPickEmail", {
+        origin: self.hostname,
+        allow_persistent: self.allowPersistent
+      });
     });
 
     subscribe("forgot_password", function(msg, info) {