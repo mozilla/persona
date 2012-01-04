77aa167c0fe1de51f248b5878d4af77119a9e636
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-01-03 17:51:41 -0700

    update all database apis on the JSON side to interact in terms of user ids as the primary identifier rather than an email address.  first step toward migrating to userid in sessions instead of email addresses to solve issue #388 (and others like it) and pave the way for primaries

--- tests/db-test.js
+++ tests/db-test.js
@@ -180,7 +180,10 @@ suite.addBatch({
 suite.addBatch({
   "checkAuth returns": {
     topic: function() {
-      db.checkAuth('lloyd@nowhe.re', this.callback);
+      var cb = this.callback;
+      db.emailToUID('lloyd@nowhe.re', function(uid) {
+        db.checkAuth(uid, cb);
+      });
     },
     "the correct password": function(r) {
       assert.strictEqual(r, "fakepasswordhash");
@@ -189,34 +192,58 @@ suite.addBatch({
 });
 
 suite.addBatch({
-  "staging an email": {
+  "emailToUID": {
     topic: function() {
-      db.stageEmail('lloyd@nowhe.re', 'lloyd@somewhe.re', this.callback);
+      db.emailToUID('lloyd@nowhe.re', this.callback);
     },
-    "yields a valid secret": function(secret) {
-      assert.isString(secret);
-      assert.strictEqual(secret.length, 48);
+    "returns a valid userid": function(r) {
+      assert.isNumber(r);
     },
-    "then": {
-      topic: function(secret) {
-        var cb = this.callback;
-        db.isStaged('lloyd@somewhe.re', function(r) { cb(secret, r); });
+    "returns a UID": {
+      topic: function(uid) {
+        db.userOwnsEmail(uid, 'lloyd@nowhe.re', this.callback);
       },
-      "makes it visible via isStaged": function(sekret, r) { assert.isTrue(r); },
-      "lets you verify it": {
-        topic: function(secret, r) {
-          db.gotVerificationSecret(secret, undefined, this.callback);
+      "that owns the original email": function(r) {
+        assert.ok(r);
+      }
+    }
+  }
+});
+
+suite.addBatch({
+  "getting a UID, then": {
+    topic: function() {
+      db.emailToUID('lloyd@nowhe.re', this.callback);
+    },
+    "staging an email": {
+      topic: function(uid) {
+        db.stageEmail(uid, 'lloyd@somewhe.re', this.callback);
+      },
+      "yields a valid secret": function(secret) {
+        assert.isString(secret);
+        assert.strictEqual(secret.length, 48);
+      },
+      "then": {
+        topic: function(secret) {
+          var cb = this.callback;
+          db.isStaged('lloyd@somewhe.re', function(r) { cb(secret, r); });
         },
-        "successfully": function(r) {
-          assert.isUndefined(r);
-        },
-        "and knownEmail": {
-          topic: function() { db.emailKnown('lloyd@somewhe.re', this.callback); },
-          "returns true": function(r) { assert.isTrue(r); }
-        },
-        "and isStaged": {
-          topic: function() { db.isStaged('lloyd@somewhe.re', this.callback); },
-          "returns false": function(r) { assert.isFalse(r); }
+        "makes it visible via isStaged": function(sekret, r) { assert.isTrue(r); },
+        "lets you verify it": {
+          topic: function(secret, r) {
+            db.gotVerificationSecret(secret, undefined, this.callback);
+          },
+          "successfully": function(r) {
+            assert.isUndefined(r);
+          },
+          "and knownEmail": {
+            topic: function() { db.emailKnown('lloyd@somewhe.re', this.callback); },
+            "returns true": function(r) { assert.isTrue(r); }
+          },
+          "and isStaged": {
+            topic: function() { db.isStaged('lloyd@somewhe.re', this.callback); },
+            "returns false": function(r) { assert.isFalse(r); }
+          }
         }
       }
     }
@@ -275,7 +302,10 @@ suite.addBatch({
 suite.addBatch({
   "removing an existing email": {
     topic: function() {
-      db.removeEmail("lloyd@somewhe.re", "lloyd@nowhe.re", this.callback);
+      var cb = this.callback;
+      db.emailToUID("lloyd@somewhe.re", function(uid) {
+        db.removeEmail(uid, "lloyd@nowhe.re", cb);
+      });
     },
     "returns no error": function(r) {
       assert.isUndefined(r);
@@ -321,7 +351,10 @@ suite.addBatch({
 suite.addBatch({
   "adding a primary email to that account": {
     topic: function() {
-      db.addPrimaryEmailToAccount("lloyd@primary.domain", "lloyd2@primary.domain", this.callback);
+      var cb = this.callback;
+      db.emailToUID('lloyd@primary.domain', function(uid) {
+        db.addPrimaryEmailToAccount(uid, "lloyd2@primary.domain", cb);
+      });
     },
     "returns no error": function(r) {
       assert.isUndefined(r);
@@ -345,7 +378,10 @@ suite.addBatch({
   },
   "adding a primary email to an account with only secondaries": {
     topic: function() {
-      db.addPrimaryEmailToAccount("lloyd@somewhe.re", "lloyd3@primary.domain", this.callback);
+      var cb = this.callback;
+      db.emailToUID('lloyd@somewhe.re', function(uid) {
+        db.addPrimaryEmailToAccount(uid, "lloyd3@primary.domain", cb);
+      });
     },
     "returns no error": function(r) {
       assert.isUndefined(r);
@@ -372,7 +408,10 @@ suite.addBatch({
 suite.addBatch({
   "adding a registered primary email to an account": {
     topic: function() {
-      db.addPrimaryEmailToAccount("lloyd@primary.domain", "lloyd3@primary.domain", this.callback);
+      var cb = this.callback;
+      db.emailToUID('lloyd@primary.domain', function(uid) {
+        db.addPrimaryEmailToAccount(uid, "lloyd3@primary.domain", cb);
+      });
     },
     "returns no error": function(r) {
       assert.isUndefined(r);
@@ -415,7 +454,10 @@ suite.addBatch({
 suite.addBatch({
   "canceling an account": {
     topic: function() {
-      db.cancelAccount("lloyd@somewhe.re", this.callback);
+      var cb = this.callback;
+      db.emailToUID("lloyd@somewhe.re", function(uid) {
+        db.cancelAccount(uid, cb);
+      });
     },
     "returns no error": function(r) {
       assert.isUndefined(r);
