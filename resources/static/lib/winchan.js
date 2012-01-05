8b199355423b345a84eff350c139f3e988c3cef7
Shane Tomlinson <stomlinson@mozilla.com>
2012-01-05 11:26:39 +0000

    Fix for infinite loop when searching for relay frame.
    close #846

--- resources/static/lib/winchan.js
+++ resources/static/lib/winchan.js
@@ -52,7 +52,7 @@
     var loc = window.location;
     var frames = window.opener.frames;
     var origin = loc.protocol + '//' + loc.host;
-    for (i = frames.length - 1; i >= 0; i++) {
+    for (var i = frames.length - 1; i >= 0; i--) {
       try {
         if (frames[i].location.href.indexOf(origin) === 0 &&
             frames[i].name === RELAY_FRAME_NAME)
