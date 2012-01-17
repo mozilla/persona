706b38dbad7ee9b15a4f6e4da9b34f1952d9cdcc
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-01-17 10:56:55 -0700

    add version.js - a small abstraction which extracts a 7char abbrev sha from the current source - issue #226

---
+++ tests/software-version-test.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+require('./lib/test_env.js');
+
+const
+assert = require('assert'),
+vows = require('vows'),
+fs = require('fs'),
+path = require('path'),
+version = require('../lib/version.js');
+
+var suite = vows.describe('software-version');
+suite.options.error = false;
+
+
+suite.addBatch({
+  "version": {
+    topic: function() { return version; },
+    "works": function(r) {
+      assert.isString(r);
+      assert.equal(r.length, 7);
+    }
+  }
+});
+
+// run or export the suite.
+if (process.argv[1] === __filename) suite.run();
+else suite.export(module);
