6498a53a82aeb39007205232da69c1c080751961
Austin King <shout@ozten.com>
2012-01-19 12:41:03 -0800

    gettext should return msgid in case of empty string

--- resources/static/shared/gettext.js
+++ resources/static/shared/gettext.js
@@ -7,7 +7,8 @@ function Gettext(params) {
       gettext: function (msgid) {
         if (json_locale_data && json_locale_data["client"]) {
         var dict = json_locale_data["client"];
-          if (dict[msgid] && dict[msgid].length >= 2) {
+          if (dict[msgid] && dict[msgid].length >= 2 &&
+              dict[msgid][1].trim() != "") {
             return dict[msgid][1];
           }
       }
