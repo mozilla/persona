1218e7ba972957ceff7e345bab9de1c43c9924db
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-01-28 12:12:43 -0700

    (aws deploy) update to new template

--- scripts/deploy/vm.js
+++ scripts/deploy/vm.js
@@ -4,7 +4,7 @@ jsel = require('JSONSelect'),
 key = require('./key.js'),
 sec = require('./sec.js');
 
-const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-51ac7d38';
+const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-7fc11016';
 
 function extractInstanceDeets(horribleBlob) {
   var instance = {};
