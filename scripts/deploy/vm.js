c3396f7f8d015a304ec3ce6c0014255a1363beed
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-01-30 15:57:30 -0700

    template update

--- scripts/deploy/vm.js
+++ scripts/deploy/vm.js
@@ -4,7 +4,7 @@ jsel = require('JSONSelect'),
 key = require('./key.js'),
 sec = require('./sec.js');
 
-const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-cd15c4a4';
+const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-7314c51a';
 
 function extractInstanceDeets(horribleBlob) {
   var instance = {};
