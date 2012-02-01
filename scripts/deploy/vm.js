546b8f52ef6060091c1ddb59149c9ad3f805f7e0
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-02-01 16:02:24 -0700

    update image, add system requirements for l10n processing (perl-JSON and perl-Locale-PO)

--- scripts/deploy/vm.js
+++ scripts/deploy/vm.js
@@ -4,7 +4,7 @@ jsel = require('JSONSelect'),
 key = require('./key.js'),
 sec = require('./sec.js');
 
-const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-1553827c';
+const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-fd558494';
 
 function extractInstanceDeets(horribleBlob) {
   var instance = {};
