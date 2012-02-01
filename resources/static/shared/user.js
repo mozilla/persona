0cd88231443e30d3e4d579e3b6f42307acb49187
Shane Tomlinson <stomlinson@mozilla.com>
2012-02-01 19:13:14 +0000

    Fixing the complete registration flow when the browser the user completes registration is different from the one they started at.
    * in check_registration.js, the "auth" message has been changed to "authenticate"
    * in user.js, when the registration poll completes, set the "known" field of the address in the addressInfo cache to "true"
    
    closes #1026, closes #973

--- resources/static/shared/user.js
+++ resources/static/shared/user.js
@@ -112,6 +112,19 @@ BrowserID.User = (function() {
           // ensure that the stagedOnBehalfOf is cleared so there is no stale
           // data.
           storage.setStagedOnBehalfOf("");
+
+          // To avoid too many address_info requests, returns from each
+          // address_info request are cached.  If the user is doing
+          // a registrationPoll, it means the user was registering the address
+          // and the registration has completed.  Because the status is
+          // "complete" or "known", we know that the address is known, so we
+          // toggle the field to be up to date.  If the known field remains
+          // false, then the user is redirected back to the authentication
+          // page and the system thinks the address must be verified again.
+          if(addressCache[email]) {
+            addressCache[email].known = true;
+          }
+
           if (onSuccess) {
             onSuccess(status);
           }
