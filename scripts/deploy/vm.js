4ce2d4017dd45d858a9fd878d3dd285089f7b5aa
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-02-02 18:00:06 -0700

    (aws deploy) repair .hacksign.in certs, new template

--- scripts/deploy/vm.js
+++ scripts/deploy/vm.js
@@ -4,7 +4,7 @@ jsel = require('JSONSelect'),
 key = require('./key.js'),
 sec = require('./sec.js');
 
-const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-f972a390';
+const BROWSERID_TEMPLATE_IMAGE_ID = 'ami-6978a900';
 
 function extractInstanceDeets(horribleBlob) {
   var instance = {};
