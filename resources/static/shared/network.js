9d2e02b7ab841c1208ae622bb6bf923d0d915f4c
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-03-12 14:33:43 -0600

    send up ephemeral bit when authenticating with primary-issued assertion as well as when authenticating with a password

--- resources/static/shared/network.js
+++ resources/static/shared/network.js
@@ -121,7 +121,8 @@ BrowserID.Network = (function() {
         url: "/wsapi/auth_with_assertion",
         data: {
           email: email,
-          assertion: assertion
+          assertion: assertion,
+          ephemeral: !storage.usersComputer.confirmed(email)
         },
         success: handleAuthenticationResponse.curry("assertion", onComplete, onFailure),
         error: onFailure
