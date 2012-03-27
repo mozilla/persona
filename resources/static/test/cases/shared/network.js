b3cbaa09b3de30dca50d7ca27f8e873ea86652c2
Shane Tomlinson <stomlinson@mozilla.com>
2012-03-27 18:53:19 +0100

    Fix cookiesEnabled being possible to call it's onComplete callback twice if onComplete throws an exception the first time.
    issue #1339

--- resources/static/test/cases/shared/network.js
+++ resources/static/test/cases/shared/network.js
@@ -548,4 +548,21 @@
     }, testHelpers.unexpectedXHRFailure);
   });
 
+  asyncTest("cookiesEnabled with onComplete exception thrown - should not call onComplete a second time", function() {
+    // Since we are manually throwing an exception, it must be caught
+    // below.
+    try {
+      network.cookiesEnabled(function(status) {
+        // if there is a problem, this callback will be called a second time
+        // with a false status.
+        equal(status, true, "cookies are enabled, correct status");
+        start();
+
+        throw "callback exception";
+      }, testHelpers.unexpectedXHRFailure);
+    } catch(e) {
+      equal(e.toString(), "callback exception", "correct exception caught");
+    }
+  });
+
 }());
