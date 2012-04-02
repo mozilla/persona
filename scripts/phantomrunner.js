dcdff7e9e9a9d1603c1cd36272c3cae10fc16b13
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-04-02 12:13:41 -0600

    increase the amount of time allotted for frontend tests to run - they can be slooow on travisci

--- scripts/phantomrunner.js
+++ scripts/phantomrunner.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const TIMEOUT_SECONDS = 120;
+const TIMEOUT_SECONDS = 160;
 
 /**
  * Wait until the test condition is true or a timeout occurs. Useful for waiting
