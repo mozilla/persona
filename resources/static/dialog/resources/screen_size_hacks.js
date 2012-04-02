ea5ce6ca84e594b292283b741b83eaa3a531575f
Shane Tomlinson <stomlinson@mozilla.com>
2012-04-02 15:11:11 +0100

    A bunch of changes to make the design more responsive to all screen sizes - mobile, desktop and tablet.
    * Start with a slight refactor of the CSS and DOM.
    * Vertical center content when content is smaller than the screen size.
    * Constrain email addresses and show ellipsis if emails overflow box width.  Make sure width is updated when browser window size changes or user changes the screen direction of their device.
    * If form/email address list is taller than available form area, show scroll bar on desktop or expand contents so that entire app must be scrolled on mobile.
    * If dialog opened on a tablet or large desktop window, add a gradient to the left size of the white area that matches the gradient on the arrow.
    * On mobile, footer should always be at the very bottom of the screen unless contents overflow screen height, where it will be pushed off the bottom of the screen.
    
    Things to test and be aware of:
    * Dialog should respond to various screen sizes when resizing browser window on desktop.
    * IE8 does not support media queries so narrowing the browser window will not force the dialog into "mobile" mode.
    * Contents in the "form area", wait, delay, or error screens should always be vertically centered.
    * IE6/IE7 should still see the unsupported dialog with the correct layout (untested on checkin due to #1390)
    * Wide emails should always be constrained to the screen width on mobile or box width on desktop.  The layout should respond to changes in device orientation.
    * The box surrounding the emails should show a scroll bar on desktop/tablet if there are more emails the available screen height.  On mobile, the entire screen height should expand so that the entire app must be scrolled.
    * On mobile, the footer should always be at the very bottom of the screen unless the form area takes up more vertical space than the device height.
    
    issue #1101
    issue #1317

---
+++ resources/static/dialog/resources/screen_size_hacks.js
@@ -0,0 +1,104 @@
+(function() {
+  /**
+   * This is a hack to feign fixed headers/footers and dynamic body content
+   * size.  On mobile, it helps keep the footer at the very bottom of the
+   * screen without the jumpiness that comes with position: fixed in both
+   * Fennec and Android native browser.  On desktop/tablet browsers, resizing
+   * the #content element causes the contents to be vertically centered.
+   */
+  function onResize() {
+    var selectEmailEl = $("#selectEmail"),
+        contentEl = $("#content");
+
+    selectEmailEl.css("position", "static");
+    if($(window).width() >= 640) {
+      // First, remove the mobile hacks
+      selectEmailEl.css("width", "");
+      contentEl.css("min-height", "");
+
+      // This is a hack for desktop mode which centers the form vertically in
+      // the middle of its container.  We have to do this hack because we use
+      // table cell vertical centering when the browserid window is large and
+      // the number of emails small, but if the screen size is smaller than the
+      // number of emails, we have to print a scrollbar - but only around the
+      // emails.
+
+      // set the height to static so that we can get the height without
+      // constraints.
+      var height = selectEmailEl.innerHeight();
+      // re-introduce constraints
+
+      if(height < $("#signIn .vertical").innerHeight()) {
+        selectEmailEl.addClass("center");
+      }
+      else {
+        selectEmailEl.removeClass("center");
+      }
+    }
+    else {
+        // First, remove the desktop hacks
+        selectEmailEl.removeClass("center");
+
+        // Hack to make sure the email addresses stay within their container.
+        // We have to do this ghettoness because table-cells (which are used to
+        // vertically center everything) expand to fully contain their children
+        // and the ellipsis never show up as expected.
+
+        // First, find the maximum width that emails can be.
+        selectEmailEl.css("width", "10px").removeClass("center");
+        var constrainedWidth = $("#signIn .contents").innerWidth();
+
+        // Find the real maximum width.
+        selectEmailEl.css("width", "");
+        var maxEmailWidth = selectEmailEl.innerWidth();
+
+        // If we have a too large an email, constrain the width.
+        if(maxEmailWidth > constrainedWidth) {
+          selectEmailEl.css("width", constrainedWidth + "px");
+        }
+
+        // Hack to find the min-height of the content area the footer is pushed
+        // to the bottom if the contents are too small, and expands off the
+        // bottom if the contents are large.
+
+        // Unconstrain everything so that we can find natural heights of all
+        // elements.
+        $("section,#signIn").css("position", "static");
+        contentEl.css("min-height", "0"); // required for Chrome to correctly resize the window
+
+        var headerHeight = $("header").outerHeight();
+        var footerHeight = $("footer").outerHeight();
+        var windowHeight = $(window).height();
+
+        // Get the amount of space between the header and footer with the
+        // caveat that we are forcing the footer to be at the bottom of the
+        // screen if the form's unconstrained height is smaller than the
+        // content area's height.
+        var contentHeight = windowHeight - headerHeight - footerHeight;
+
+        // Get the natural height of the form
+        var formHeight = $("#formWrap").outerHeight();
+
+        // set the min height of the content area.  This serves two purposes.
+        // First off, for accounts with only one or two emails, it will ensure
+        // that the footer is at the bottom of the mobile screen with the
+        // emails (or any other form) vertically centered on the screen.
+        // Secondly, if an account has many many emails, it will ensure the
+        // content area expands correctly to keep any email addresses from
+        // being hidden.  This means the footer will be off the screen and the
+        // mobile user must scroll the entire content area up and down
+        // - contrast this to the desktop version where users with many email
+        // addresses only have to scroll the list of emails.
+        contentHeight = Math.max(100, contentHeight, formHeight);
+        contentEl.css("min-height", contentHeight + "px");
+
+        $("section,#signIn").css("position", "");
+    }
+
+    selectEmailEl.css("position", "");
+  }
+
+  $(window).resize(onResize);
+  onResize();
+  BrowserID.resize = onResize;
+}());
