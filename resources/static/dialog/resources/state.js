cd74172cf0795ab6f2fff7d483eb742d3c5a3edd
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-04-27 11:30:05 -0600

    comment wordsmithing

--- resources/static/dialog/resources/state.js
+++ resources/static/dialog/resources/state.js
@@ -242,12 +242,12 @@ BrowserID.State = (function() {
 
     handleState("reset_password", function(msg, info) {
       info = info || {};
-      // reset password says the password user has confirmed that they want to
-      // reset their password.  doResetPassword will attempt to call the
-      // call the create user wsapi call.  If the wsapi call is successful,
-      // the user will be directed to the "go verify your account" message.
+      // reset_password says the user has confirmed that they want to
+      // reset their password.  doResetPassword will attempt to invoke
+      // the create_user wsapi.  If the wsapi call is successful,
+      // the user will be shown the "go verify your account" message.
 
-      // We have to save off the staged email address here for when the user
+      // We have to save the staged email address here for when the user
       // verifies their account and user_confirmed is called.
       self.stagedEmail = info.email;
       startAction(false, "doResetPassword", info);
