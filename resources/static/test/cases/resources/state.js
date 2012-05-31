47f0c10a06655436880434ee0f9c2def87c807f4
Shane Tomlinson <stomlinson@mozilla.com>
2012-05-31 17:02:34 +0100

    Clean up after the rebase.

--- resources/static/test/cases/resources/state.js
+++ resources/static/test/cases/resources/state.js
@@ -141,7 +141,6 @@
       privacyURL: "https://browserid.org/priv.html"
     });
 
-    ok(actions.info.doRPInfo.sitename, "doRPInfo called with sitename added and set");
     ok(actions.info.doRPInfo.tosURL, "doRPInfo called with tosURL set");
     ok(actions.info.doRPInfo.privacyURL, "doRPInfo called with privacyURL set");
   });
