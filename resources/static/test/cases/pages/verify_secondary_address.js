16947d1d6ba98ef9f5b71192f7fd3d0377f6f5c3
Shane Tomlinson <stomlinson@mozilla.com>
2012-06-15 15:08:56 +0100

    Fix the failing test.

--- resources/static/test/cases/pages/verify_secondary_address.js
+++ resources/static/test/cases/pages/verify_secondary_address.js
@@ -78,7 +78,7 @@
     createController(config, function() {
       testVisible("#congrats");
       testHasClass("body", "complete");
-      equal($("#congrats .website").text(), returnTo, "website is updated");
+      equal($(".website").text(), returnTo, "website is updated");
       equal(doc.location.href, returnTo, "redirection occurred to correct URL");
       equal(storage.getLoggedIn("https://test.domain"), "testuser@testuser.com", "logged in status set");
       start();
