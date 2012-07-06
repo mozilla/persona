cbf2e7d9d9da00914b6e98cf6eeb6a1541e90ca5
Ben Adida <ben@adida.net>
2012-07-06 14:50:57 -0700

    special case auth_with_primary so that we have context and we redirect appropriately

--- resources/static/auth_with_idp/main.js
+++ resources/static/auth_with_idp/main.js
@@ -5,7 +5,13 @@
 var wc = WinChan.onOpen(function(origin, args, cb) {
   if (window.location.hash === '#complete') cb();
   else {
+    var fullURL = args;
+
+    // store information in window.name to indicate that
+    // we redirect here
+    window.name = 'auth_with_primary';
+
     wc.detach();
-    window.location = args;
+    window.location = fullURL;
   }
 });
