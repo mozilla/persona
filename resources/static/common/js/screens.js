bdaa046412857d0880e379434956b4ba7fdb840c
Shane Tomlinson <stomlinson@mozilla.com>
2012-07-09 15:14:44 +0100

    if there are old extendedInfo screens hanging around when a screen is shown, tear it down

--- resources/static/common/js/screens.js
+++ resources/static/common/js/screens.js
@@ -22,6 +22,12 @@ BrowserID.Screens = (function() {
         // extendedInfo takes care of info that is on a screen but hidden by
         // default.  When the user clicks the "open extended info" button, it
         // is displayed to them.
+
+        if (self.extendedInfo) {
+          // sometimes a screen is overwritten and never hidden.  When this
+          // happens, old extendedInfos need to be torn down.
+          self.extendedInfo.stop();
+        }
         self.extendedInfo = bid.Modules.ExtendedInfo.create();
         self.extendedInfo.start({ target: target });
 
