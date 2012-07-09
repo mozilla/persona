2d16a83224fafc88820869069c33c9c365118fee
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-07-09 13:55:19 -0600

    disable debug language in our dev env for now.  it was never working right, and was relying on obsolete .json files.  Now that those are gone (see PR #1918), we can see that things were broken.  See issue #1970 to track the fix

--- scripts/awsbox/post_deploy.js
+++ scripts/awsbox/post_deploy.js
@@ -8,22 +8,22 @@ else
     echo ">> no keypair needed.  you gots one"
 fi
 
-echo ">> updating strings"
-svn co -q http://svn.mozilla.org/projects/l10n-misc/trunk/browserid/locale
-cd locale
-svn up
-cd ..
-./scripts/extract_po.sh locale/
-# yuck!  our debug language breaks if this is not present
-for file in locale/templates/LC_MESSAGES/*.pot ; do
-    mv $file $file.old
-    sed 's/CHARSET/UTF-8/g' $file.old > $file
-    rm -f $file.old
-done
-
-./scripts/merge_po.sh locale/
-./locale/compile-mo.sh locale/
-./locale/compile-json.sh locale/ resources/static/i18n/
+#echo ">> updating strings"
+#svn co -q http://svn.mozilla.org/projects/l10n-misc/trunk/browserid/locale
+#cd locale
+#svn up
+#cd ..
+#./scripts/extract_po.sh locale/
+## yuck!  our debug language breaks if this is not present
+#for file in locale/templates/LC_MESSAGES/*.pot ; do
+#    mv $file $file.old
+#    sed 's/CHARSET/UTF-8/g' $file.old > $file
+#    rm -f $file.old
+#done
+#
+#./scripts/merge_po.sh locale/
+#./locale/compile-mo.sh locale/
+#./locale/compile-json.sh locale/ resources/static/i18n/
 
 echo ">> generating production resources"
 scripts/compress
