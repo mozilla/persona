0e323943f48d2c429d1606e5cad2c9e646984a74
Shane Tomlinson <stomlinson@mozilla.com>
2012-07-16 14:02:10 +0100

    Update frontend ejs library to deal with <%-
    <%- is an alias for <%=.
    
    Update one unit test so all frontend tests pass.

--- resources/static/test/cases/pages/js/verify_secondary_address.js
+++ resources/static/test/cases/pages/js/verify_secondary_address.js
@@ -84,7 +84,7 @@
     createController(config, function() {
       testVisible("#congrats");
       testHasClass("body", "complete");
-      equal($(".website").text(), returnTo, "website is updated");
+      equal($(".website").eq(0).text(), returnTo, "website is updated");
       equal(doc.location.href, returnTo, "redirection occurred to correct URL");
       equal(storage.getLoggedIn("https://test.domain"), "testuser@testuser.com", "logged in status set");
       start();
