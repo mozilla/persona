wbamberg/browserid

authenticate_user: factor out updateHash code
After the user is authenticated, we might update the hashed password if
the bcrypt work factor has been changed. To simplify the code in
preparation for later changes, this patch extracts the updateHash() code
out to a separate function.

Note that this function must run *after* the `res.json()` call for two
reasons. The first is to avoid slowing down the client (send answer
first, do work later). The second is to get the right session cookie
into the POST that we send to ourselves (to /wsapi/update_password) that
does the actual hash updated. The session is updated in
wsapi.authenticateSession, but the cookie isn't regenerated until the
call to `res.json()`, and the POST is sneakily grabbing the cookie out
of the response's Set-Cookie header to copy into the request-to-self.

lib/wsapi/authenticate_user.js
@@ -70,59 +70,63 @@ exports.process = function(req, res) {
                                       config.get('ephemeral_session_duration_ms')
                                       : config.get('authentication_duration_ms')
                                     }, function(err) {
-                                     if (err)
-                                       return wsapi.databaseDown(res, err);
-                                     res.json({ success: true, userid: uid });
-                                    });
+            if (err)
+              return wsapi.databaseDown(res, err);
+            res.json({ success: true, userid: uid });
 
+            // if the work factor has changed, update the hash here.  issue #204
+            // NOTE: this runs asynchronously and will not delay the response
+            if (config.get('bcrypt_work_factor') != bcrypt.get_rounds(hash))
+              updateHash(req, res, uid, hash);
+          });
 
-          // if the work factor has changed, update the hash here.  issue #204
-          // NOTE: this runs asynchronously and will not delay the response
-          if (config.get('bcrypt_work_factor') != bcrypt.get_rounds(hash)) {
-            logger.info("updating bcrypted password for user " + uid);
-
-            // this request must be forwarded to dbwriter, and we'll use the
-            // authentication cookie of the user just sent out.
-            var u = wsapi.forwardWritesTo;
-
-            var m = u.scheme === 'http' ? http : https;
-
-            var post_body = querystring.stringify({
-              oldpass: req.params.pass,
-              newpass: req.params.pass,
-              csrf: req.params.csrf
-            });
-            var preq = m.request({
-              host: u.host,
-              port: u.port,
-              path: '/wsapi/update_password',
-              method: "POST",
-              headers: {
-                'Cookie': res._headers['set-cookie'],
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': post_body.length
-              }
-            }, function(pres) {
-              pres.on('end', function() {
-                if (pres.statusCode !== 200) {
-                  logger.error("failed to update bcrypt rounds of password for " + uid +
-                               " dbwriter returns " + pres.statusCode);
-                } else {
-                  logger.info("bcrypt rounds of password for " + uid +
-                              " successfully updated (from " +
-                              bcrypt.get_rounds(hash) + " to "
-                              + config.get('bcrypt_work_factor') + ")");
-                }
-              });
-            }).on('error', function(e) {
-              logger.error("failed to update bcrypt rounds of password for " + uid + ": " + e);
-            });
-
-            preq.write(post_body);
-            preq.end();
-          }
         }
       });
     });
   });
 };
+
+
+function updateHash(req, res, uid, hash) {
+  logger.info("updating bcrypted password for user " + uid);
+
+  // this request must be forwarded to dbwriter, and we'll use the
+  // authentication cookie of the user just sent out.
+  var u = wsapi.forwardWritesTo;
+
+  var m = u.scheme === 'http' ? http : https;
+
+  var post_body = querystring.stringify({
+    oldpass: req.params.pass,
+    newpass: req.params.pass,
+    csrf: req.params.csrf
+  });
+  var preq = m.request({
+    host: u.host,
+    port: u.port,
+    path: '/wsapi/update_password',
+    method: "POST",
+    headers: {
+      'Cookie': res._headers['set-cookie'],
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': post_body.length
+    }
+  }, function(pres) {
+    pres.on('end', function() {
+      if (pres.statusCode !== 200) {
+        logger.error("failed to update bcrypt rounds of password for " + uid +
+                     " dbwriter returns " + pres.statusCode);
+      } else {
+        logger.info("bcrypt rounds of password for " + uid +
+                    " successfully updated (from " +
+                    bcrypt.get_rounds(hash) + " to "
+                    + config.get('bcrypt_work_factor') + ")");
+      }
+    });
+  }).on('error', function(e) {
+    logger.error("failed to update bcrypt rounds of password for " + uid + ": " + e);
+  });
+
+  preq.write(post_body);
+  preq.end();
+}

mozilla/persona

lib/wsapi/authenticate_user.js
@@ -75,54 +75,57 @@ exports.process = function(req, res) {
                                      res.json({ success: true, userid: uid });
                                     });
 
-
           // if the work factor has changed, update the hash here.  issue #204
           // NOTE: this runs asynchronously and will not delay the response
-          if (config.get('bcrypt_work_factor') != bcrypt.getRounds(hash)) {
-            logger.info("updating bcrypted password for user " + uid);
-
-            // this request must be forwarded to dbwriter, and we'll use the
-            // authentication cookie of the user just sent out.
-            var u = wsapi.forwardWritesTo;
-
-            var m = u.scheme === 'http' ? http : https;
-
-            var post_body = querystring.stringify({
-              oldpass: req.params.pass,
-              newpass: req.params.pass,
-              csrf: req.params.csrf
-            });
-            var preq = m.request({
-              host: u.host,
-              port: u.port,
-              path: '/wsapi/update_password',
-              method: "POST",
-              headers: {
-                'Cookie': res._headers['set-cookie'],
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': post_body.length
-              }
-            }, function(pres) {
-              pres.on('end', function() {
-                if (pres.statusCode !== 200) {
-                  logger.error("failed to update bcrypt rounds of password for " + uid +
-                               " dbwriter returns " + pres.statusCode);
-                } else {
-                  logger.info("bcrypt rounds of password for " + uid +
-                              " successfully updated (from " +
-                              bcrypt.getRounds(hash) + " to "
-                              + config.get('bcrypt_work_factor') + ")");
-                }
-              });
-            }).on('error', function(e) {
-              logger.error("failed to update bcrypt rounds of password for " + uid + ": " + e);
-            });
-
-            preq.write(post_body);
-            preq.end();
-          }
+          if (config.get('bcrypt_work_factor') != bcrypt.get_rounds(hash))
+            updateHash(req, res, uid, hash);
         }
       });
     });
   });
 };
+
+
+function updateHash(req, res, uid, hash) {
+  logger.info("updating bcrypted password for user " + uid);
+
+  // this request must be forwarded to dbwriter, and we'll use the
+  // authentication cookie of the user just sent out.
+  var u = wsapi.forwardWritesTo;
+
+  var m = u.scheme === 'http' ? http : https;
+
+  var post_body = querystring.stringify({
+    oldpass: req.params.pass,
+    newpass: req.params.pass,
+    csrf: req.params.csrf
+  });
+  var preq = m.request({
+    host: u.host,
+    port: u.port,
+    path: '/wsapi/update_password',
+    method: "POST",
+    headers: {
+      'Cookie': res._headers['set-cookie'],
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': post_body.length
+    }
+  }, function(pres) {
+    pres.on('end', function() {
+      if (pres.statusCode !== 200) {
+        logger.error("failed to update bcrypt rounds of password for " + uid +
+                     " dbwriter returns " + pres.statusCode);
+      } else {
+        logger.info("bcrypt rounds of password for " + uid +
+                    " successfully updated (from " +
+                    bcrypt.getRounds(hash) + " to "
+                    + config.get('bcrypt_work_factor') + ")");
+      }
+    });
+  }).on('error', function(e) {
+    logger.error("failed to update bcrypt rounds of password for " + uid + ": " + e);
+  });
+
+  preq.write(post_body);
+  preq.end();
+}