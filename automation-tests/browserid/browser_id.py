579f33182e67d88f792540444947e5e9068152e5
Leah Klearman <lklrmn@gmail.com>
2012-08-31 12:55:50 -0700

    BrowserID.persona_test_user() with unit tests
    this has not been plumbed into any actual tests yet

--- automation-tests/browserid/browser_id.py
+++ automation-tests/browserid/browser_id.py
@@ -4,6 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import urllib2
+import json
+
 import selenium
 
 
@@ -23,3 +26,46 @@ class BrowserID(object):
         from pages.sign_in import SignIn
         sign_in = SignIn(self.selenium, timeout=self.timeout, expect='new')
         sign_in.sign_in(email, password)
+
+    def persona_test_user(self, verified=True, env='prod'):
+        '''
+        Create a test user.
+
+        ::Args::
+        - verified - boolean True/False should the user be verified
+        - env      - string dev/stage/prod instance of persona.org used by 
+                     the system under test(default prod)
+
+        ::Returns::
+        A dictionary that combines the values returned by the personatestuser API
+        and the values returned by browserid.mocks.MockUser.
+
+        {
+            'email': 'lopez401@personatestuser.org'
+            'primary_email': 'lopez401@personatestuser.org', 
+            'pass': 'SOaUo9qJqYyBl1sN', 
+            'password': 'SOaUo9qJqYyBl1sN', 
+            'expires': '1346445745', 
+            'verifier': 'https://verifier.dev.anosrep.org',
+            'browserid': 'https://login.dev.anosrep.org', 
+            'token': 'U6bFrRZJrZggwkJ0gkpvC9tuNNaIXpvEZM11gzLnw9l4o4UK', # for verified=False only
+            'env': 'dev', 
+        }
+
+        '''
+        command = ''
+        if verified:
+            command = 'email'
+        else:
+            command = 'unverified_email'
+
+        response = urllib2.urlopen(
+            'http://personatestuser.org/%s/%s' %
+            (command, env), timeout=self.timeout)
+
+        user = json.loads(response.read())
+        user['password'] = user['pass']
+        user['primary_email'] = user['email']
+        user.pop('events')
+        print user
+        return user
