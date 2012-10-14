a5fdda728f63d81a00ef7c784ba50f3519e6b929
John Morrison <jrgmorrison@gmail.com>
2012-10-14 16:00:17 -0700

    pull out details on which qunit tests failed (if any)

--- automation-tests/tests/frontend-qunit-test.js
+++ automation-tests/tests/frontend-qunit-test.js
@@ -10,21 +10,17 @@ utils = require('../lib/utils.js'),
 vowsHarness = require('../lib/vows_harness.js'),
 testSetup = require('../lib/test-setup.js');
 
-/*
-  This test does not currently succeed via Saucelabs for XP IE8, but 
-  does succeed for other browser/OS combinations on Sauce.
-*/
-
-// Intentionally hardcoded to dev instance. This test cannot be run in 
-// either stage or prod. (Although can be run against ephemeral).
-var devTestUrl = 'https://login.dev.anosrep.org/test/';
+// Intentionally hardcoded to dev instance. This test cannot be run in
+// either stage or prod. (Although can be run against ephemeral via env).
+var frontendTestUrl = (process.env.PERSONA_FRONTEND_TESTURL ||
+                       'https://login.dev.anosrep.org/test/');
 
 // The test page has jquery and underscore available.
 var queryResult = [
   "(function() {                                               ",
   "  var result = $('#qunit-testresult').text();               ",
-  "  if (result.indexOf('Tests completed')=== -1) {            ",
-  "    return JSON.stringify({});                              ",
+  "  if (result.indexOf('Tests completed') === -1) {           ",
+  "    return '{}';                                            ",
   "  }                                                         ",
   "  var elapsed = result.match(/(\\d+)\\s+millis/)[1];        ",
   "  var passFail =                                            ",
@@ -41,6 +37,14 @@ var queryResult = [
   "})();                                                       ",
 ].join(' ').replace(/\s+/g, ' ');
 
+var queryFailures = [
+  "(function() {                                               ",
+  "  var failures = $('#qunit-tests > li.fail > strong')       ",
+  "    .map(function() { return $(this).text() }).get();       ",
+  "  return JSON.stringify(failures);                          ",
+  "})();                                                       ",
+].join(' ').replace(/\s+/g, ' ');
+
 // pull in test environment, including wd
 var browser;
 testSetup.setup({b:1}, function(err, fix) {
@@ -52,24 +56,32 @@ vowsHarness({
     browser.newSession(testSetup.sessionOpts, done);
   },
   "open the frontend test url": function(done) {
-    browser.get(devTestUrl, done);
+    browser.get(frontendTestUrl, done);
   },
   "waitFor and check/complete the result": function(done) {
-    function check(checkCb) {
+    var check = function check(checkCb) {
+      /*jshint evil:true */
       browser.eval(queryResult, function(err, res) {
         if (err) throw err;
         if (res === '{}') return checkCb(false);
-        return checkCb(true, res);
+        browser.eval(queryFailures, function(err, failures) {
+          if (err) throw err;
+          return checkCb(true, {
+            result: JSON.parse(res),
+            failures: JSON.parse(failures),
+          });
+        });
       });
     };
-    function complete(res) {
-      assert.ok(!!res, 'The test returned a value');
-      res = JSON.parse(res);
+    var complete = function complete(res) {
       //XXX should have a better way to save off the elapsed time, test
       // counts, pass/fail, etc., somewhere better than just dumping it in the
       // console. Maybe set custom-data for Sauce after the job completes?
+      // - Should have a mode here where we just return the `res` back to 
+      // a parallel test runner, which can then make assertions about
+      // all the results.
       console.dir(res);
-      assert.strictEqual(res.failed, '0', 'no tests failed');
+      assert.equal(res.result.failed, '0', 'no tests failed');
       done();
     };
     // The frontend tests take at least 20 seconds to run locally (FF); Chrome
