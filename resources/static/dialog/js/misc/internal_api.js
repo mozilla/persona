6eb26d49dc91a2eaca3b96d6aa9c812b224932ea
Zachary Carter <zack.carter@gmail.com>
2012-10-18 17:22:43 -0700

    We need to set the origin here

--- resources/static/dialog/js/misc/internal_api.js
+++ resources/static/dialog/js/misc/internal_api.js
@@ -178,6 +178,7 @@
     log('internal watch options', options);
     var remoteOrigin = options.origin;
     var loggedInUser = options.loggedInUser;
+    user.setOrigin(remoteOrigin);
 
     function checkAndEmit() {
       log('checking and emitting');
