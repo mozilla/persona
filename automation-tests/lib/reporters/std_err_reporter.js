2120755988f8e1f8f682d9e394fac140a88a4ae2
Shane Tomlinson <stomlinson@mozilla.com>
2012-11-01 16:02:10 +0000

    Write directly to stderr and stdout instead of using the util functionality. This results in fewer newlines being added to the output.

--- automation-tests/lib/reporters/std_err_reporter.js
+++ automation-tests/lib/reporters/std_err_reporter.js
@@ -2,11 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const util = require('util');
-
 function StdErrReporter() { }
 StdErrReporter.prototype.report = function(msg) {
-  util.error(msg);
+  process.stderr.write(msg);
 };
 StdErrReporter.prototype.done = function() {};
 
--- automation-tests/lib/reporters/std_out_reporter.js
+++ automation-tests/lib/reporters/std_out_reporter.js
@@ -2,11 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const util = require('util');
-
 function StdOutReporter() { }
 StdOutReporter.prototype.report = function(msg) {
-  util.print(msg);
+  process.stdout.write(msg);
 };
 StdOutReporter.prototype.done = function() {};
 
