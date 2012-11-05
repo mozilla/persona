48a8898e9e8190591feecaaa54e25f6224879478
Shane Tomlinson <stomlinson@mozilla.com>
2012-11-05 16:28:55 +0000

    Add the ability to run all tests against all supported SAUCE browsers by specifying PERSONA_BROWSER=all when running run-all.js

--- automation-tests/scripts/run-all.js
+++ automation-tests/scripts/run-all.js
@@ -26,12 +26,88 @@ const path = require('path'),
       vows_path = path.join(__dirname, "../node_modules/.bin/vows"),
       vows_args = process.env['VOWS_ARGS'] || ["--xunit", "-i"], // XXX is it cool to expect an array?
       result_extension = process.env['RESULT_EXTENSION'] || "xml",
+      platform = process.env['PERSONA_BROWSER'] || 'vista_chrome',
+      supported_platforms = require('../lib/sauce-platforms').platforms,
       start_time = new Date().getTime();
 
-function runTest(testName, testPath, stdOutReporter, stdErrReporter, done) {
-  util.puts("starting " + testName);
 
-  var testProcess = child_process.spawn(vows_path, [testPath].concat(vows_args));
+// General flow
+// 1. Get the list of tests for the specified platforms
+// 2. Run the tests in parallel with a max of max_runners jobs.
+// 3. Report the results.
+
+// the tests array is shared state across all invocations of runNext. If two or
+// more tests are run in parallel, they will all check the tests array to see
+// if there are any more tests to run.
+var tests = getTheTests(getTestedPlatforms(platform));
+console.log("Running", tests.length, "suites across", max_runners, "runners");
+runTests();
+
+
+
+
+
+
+
+function getTestedPlatforms(platform) {
+  if (platform === 'all') return supported_platforms;
+
+  var platforms = {};
+  platforms[platform] = supported_platforms[platform];
+  return platforms;
+}
+
+function getTheTests(platforms) {
+  var testSet = test_finder.find(),
+      allTests = [];
+
+  allTests = [];
+
+  // make a copy of the test set for each platform, set the platform of each
+  // test, and append the platform specific test set to overall list of
+  // tests.
+  for (var key in platforms) {
+    // The JSON.stringify/JSON.parse creates a deep copy of the testSet so that
+    // we can modify it without worrying about shared properties.
+    var platformTests = [].concat(JSON.parse(JSON.stringify(testSet)));
+    setPlatformOfTests(platformTests, key);
+    allTests = allTests.concat(platformTests);
+  }
+
+  return allTests;
+}
+
+function setPlatformOfTests(tests, platform) {
+  tests.forEach(function(test) {
+    test.platform = platform;
+  });
+}
+
+function runTest(test, stdOutReporter, stdErrReporter, done) {
+  var testName = test.name,
+      testPath = test.path,
+      platform = test.platform;
+
+  util.puts("starting " + testName + " on " + platform);
+
+
+  // make a copy of the current process' environment but force the
+  // platform.
+  var environment = {};
+  Object.keys(process.env).forEach(function(key) {
+    environment[key] = process.env[key];
+  });
+  if (platform) {
+    environment.PERSONA_BROWSER = platform;
+  }
+
+  var opts = {
+    cwd: undefined,
+    env: environment
+  };
+
+  var testProcess = child_process.spawn(vows_path,
+    [testPath].concat(vows_args), opts);
 
   testProcess.stdout.on('data', function (data) {
     stdOutReporter.report(data.toString());
@@ -47,27 +123,22 @@ function runTest(testName, testPath, stdOutReporter, stdErrReporter, done) {
   });
 };
 
-// the tests array is shared state across all invocations of runNext. If two or
-// more tests are run in parallel, they will all check the tests array to see
-// if there are any more tests to run.
-var tests = test_finder.find();
-console.log("Running", tests.length, "suites across", max_runners, "runners");
-runTests(tests);
-
 function runNext() {
   var test = tests.shift();
 
   if (test) {
-    var testPath = test.path;
-    var testName = test.name;
+    var testName = test.name,
+        platform = test.platform;
+
+    var outputPath = path.join(__dirname, '..', 'results',
+        start_time + "-" + testName + '-' + platform + '.' + result_extension);
 
     var stdOutReporter = new FileReporter({
-      output_path: path.join(__dirname, '..', 'results',
-        start_time + "-" + testName + '.' + result_extension)
+      output_path: outputPath
     });
     var stdErrReporter = new StdErrReporter();
 
-    runTest(testName, testPath, stdOutReporter, stdErrReporter, function() {
+    runTest(test, stdOutReporter, stdErrReporter, function() {
       stdOutReporter.done();
       stdErrReporter.done();
 
@@ -76,7 +147,7 @@ function runNext() {
   }
 };
 
-function runTests(tests) {
+function runTests() {
   // run tests in parallel up to the maximum number of runners.
   for(var i = 0; i < max_runners; ++i) {
     runNext();
