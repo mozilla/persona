3daa2b605c9e3bbca658f295afaca8566dbcc037
Jared Hirsch <ohai@6a68.net>
2012-12-17 08:18:04 -0800

    Re-enable all platforms for home stretch

--- automation-tests/config/sauce-platforms.js
+++ automation-tests/config/sauce-platforms.js
@@ -1,6 +1,5 @@
 // platforms supported by sauce that we test
 const platforms = {
-  /*
   "linux_firefox_16": {
     platform: 'LINUX',
     browserName: 'firefox',
@@ -15,7 +14,7 @@ const platforms = {
     platform: 'MAC',
     browserName:'firefox',
     version:'14'
-  },*/
+  },
   "vista_chrome": {
     platform:'VISTA',
     browserName:'chrome'
@@ -29,22 +28,22 @@ const platforms = {
     platform:'VISTA',
     browserName:'internet explorer',
     version:'9'
-  },/*
+  },
   "win8_ie_10": {
     platform: 'Windows 2012',
     browserName: 'internet explorer',
     version: '10'
-  },*/
+  },
   "xp_ie_8": {
     platform:'XP',
     browserName: 'internet explorer',
     version:'8'
-  }/*,
+  },
   "osx_safari_5": {
     platform:'Mac 10.6',
     browserName: 'safari',
     version:'5'
-  }*/
+  }
 };
 
 // see http://saucelabs.com/docs/ondemand/additional-config for other opts
