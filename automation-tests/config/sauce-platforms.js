9e4007a349de18d9988fc81e5bf9181165ea3d81
John Morrison <jrgmorrison@gmail.com>
2013-03-02 18:24:23 -0800

    group by browser type; no version changes

--- automation-tests/config/sauce-platforms.js
+++ automation-tests/config/sauce-platforms.js
@@ -1,28 +1,33 @@
 // platforms supported by sauce that we test
 const platforms = {
+  // Firefox
+  "vista_firefox_16": {
+    platform:'VISTA',
+    browserName:'firefox',
+    version:'16'
+  },
   "linux_firefox_16": {
     platform: 'LINUX',
     browserName: 'firefox',
     version: '16'
-  },
-  "linux_opera_12": {
-    platform: 'LINUX',
-    browserName: 'opera',
-    version: '12'
   },
   "osx_firefox_14": {
     platform: 'MAC',
     browserName:'firefox',
     version:'14'
   },
+
+  // Chrome
   "vista_chrome": {
     platform:'VISTA',
     browserName:'chrome'
   },
-  "vista_firefox_16": {
-    platform:'VISTA',
-    browserName:'firefox',
-    version:'16'
+
+  // IE
+  "xp_ie_8": {
+    platform:'XP',
+    browserName: 'internet explorer',
+    version:'8'
   },
   "vista_ie_9": {
     platform:'VISTA',
@@ -34,11 +39,15 @@ const platforms = {
     browserName: 'internet explorer',
     version: '10'
   },
-  "xp_ie_8": {
-    platform:'XP',
-    browserName: 'internet explorer',
-    version:'8'
+
+  // Opera
+  "linux_opera_12": {
+    platform: 'LINUX',
+    browserName: 'opera',
+    version: '12'
   },
+
+  // Safari
   "osx_safari_5": {
     platform:'Mac 10.6',
     browserName: 'safari',
