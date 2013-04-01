a9509a2b337830b12beee99cc37abdd9530ca5f4
Jared Hirsch <ohai@6a68.net>
2013-04-01 14:05:49 -0700

    Revert "Make sure the title and message for getErrorDialog are properly escaped when displayed."
    This reverts commit 5b32450e84ff2507196471f86fa9e64036099d23.
    
    Signed-off-by: Jared Hirsch <ohai@6a68.net>

--- resources/static/test/cases/common/js/modules/page_module.js
+++ resources/static/test/cases/common/js/modules/page_module.js
@@ -107,25 +107,16 @@
   asyncTest("getErrorDialog gets a function that can be used to render an error message", function() {
     createController();
 
-    function escape(text) {
-      var escaped = _.escape(text);
-      // underscore escapes `/` whereas ejs does not
-      return escaped.replace(/&#x2F;/g, '/');
-    }
-
-    // Because these can come from the IdP, they must be escaped when displayed
-    // or else the IdP could XSS users
-    var title = "<span>error title</span>";
-    var message = "<span>extended error message</span>";
+    var title = "error title";
+    var message = "extended error message";
 
     // Call getErrorDialog with the "action" info.
     var func = controller.getErrorDialog({
       title: title
     }, function() {
       testErrorVisible();
-      // make sure the text is escaped before IdPs start XSSing users.
-      equal($("#errorTitle").html().trim(), escape(title));
-      equal($("#errorMessage").html().trim(), escape(message));
+      equal($("#errorTitle").text().trim(), title);
+      equal($("#errorMessage").text().trim(), message);
       start();
     });
 
