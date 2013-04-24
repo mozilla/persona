6e697af50446067929c671d1d745cf87f5f1e4e3
Jared Hirsch <ohai@6a68.net>
2013-04-23 18:53:11 -0700

    WIP commit - hacking on timeouts for phantomJS
        It seems that phantomjs responds more quickly than other browsers; hacking
        the implicit timeout by cranking it up actually gets a few of the tests
        working :-)
    
        If this is indeed the right fix, need to abstract out the difference between
        phantom and other browsers. Not sure quite how to do this yet, hence the WIP
        commit.

--- automation-tests/lib/timeouts.js
+++ automation-tests/lib/timeouts.js
@@ -1,6 +1,8 @@
-exports.DEFAULT_POLL_MS = 500;
+exports.DEFAULT_POLL_MS = 5000;
 exports.DEFAULT_TIMEOUT_MS = 40000;
 // At the advice of the WebQA team, setting the default implicit wait to 0 and
 // using explicit waits. This helps reduce the number of errors due to explicit
 // timeouts.
-exports.DEFAULT_IMPLICIT_WAIT_MS = 0;
+
+// nope. try bumping implicit wait for phantom to wait for webkit.
+exports.DEFAULT_IMPLICIT_WAIT_MS = 2000;
