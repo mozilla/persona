fliex/browserid

Rebased dev and resolved conflicts in these files:
    resources/static/common/js/user.js
    resources/static/dialog/js/misc/helpers.js
    resources/static/dialog/js/modules/authenticate.js
    resources/static/test/cases/common/js/user.js

resources/static/dialog/js/modules/dialog.js
@@ -305,6 +305,13 @@ BrowserID.Modules.Dialog = (function() {
           user.setReturnTo(returnTo);
         }
 
+        // forceIsuser is used by the Marketplace to disable primary support
+        // and replace fxos.login.persona.org as the issuer of certs
+        if (paramsFromRP.forceIssuer) {
+          // TODO check for valid domain
+          params.forceIssuer = paramsFromRP.forceIssuer;
+	}
+
         // forceAuthentication is used by the Marketplace to ensure that the
         // user knows the password to this account. We ignore any active session.
         if (paramsFromRP.forceAuthentication &&

mozilla/persona

Conflicts:
	example/rp/index.html
	resources/static/dialog/js/misc/helpers.js
	resources/static/dialog/js/modules/dialog.js

resources/static/dialog/js/modules/dialog.js
@@ -264,6 +264,13 @@ BrowserID.Modules.Dialog = (function() {
           params.forceAuthentication = true;
         }
 
+        // forceIsuser is used by the Marketplace to disable primary support
+        // and replace fxos.login.persona.org as the issuer of certs
+        if (paramsFromRP._experimental_forceIssuer) {
+          // TODO check for valid domain
+          params.forceIssuer = paramsFromRP._experimental_forceIssuer;
+        }
+
         if (hash.indexOf("#AUTH_RETURN") === 0) {
           var primaryParams = JSON.parse(win.sessionStorage.primaryVerificationFlow);
           params.email = primaryParams.email;