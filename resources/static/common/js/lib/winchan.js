eacbeb017166698026a4b5a6f4eb3370c48a2fa4
Francois Marier <francois@mozilla.com>
2013-08-09 16:02:45 +1200

    Check each component of the relay frame origin separately
    The current check against the origin we craft will succeed in the
    case of a URL which merely starts with the same text as the
    crafted origin.
    
    An example is a URL like "http://example.com@hostile.com" which
    would match an origin like "http://example.com".
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=878485

--- resources/static/common/js/lib/winchan.js
+++ resources/static/common/js/lib/winchan.js
@@ -63,10 +63,10 @@
   function findRelay() {
     var loc = window.location;
     var frames = window.opener.frames;
-    var origin = loc.protocol + '//' + loc.host;
     for (var i = frames.length - 1; i >= 0; i--) {
       try {
-        if (frames[i].location.href.indexOf(origin) === 0 &&
+        if (frames[i].location.protocol === window.location.protocol &&
+            frames[i].location.host === window.location.host &&
             frames[i].name === RELAY_FRAME_NAME)
         {
           return frames[i];
