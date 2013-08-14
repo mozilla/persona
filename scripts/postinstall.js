822b997ef1612b59bec43006e63b0ba5c8ba58c2
Peter deHaan <pdehaan@mozilla.com>
2013-08-14 11:31:50 -0700

    Adding copyright headers to fix Issue #3715

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,9 @@
 "use strict";
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // make symlinks
 var fs = require('fs');
 var path = require('path');
--- scripts/show_config.js
+++ scripts/show_config.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var path = require('path');
 
 // use the 'local' configuration if one isn't explicitly specified in the environment
