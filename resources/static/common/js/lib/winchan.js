780bee530f86122c48357474100b0e5c4d2cd0e2
Shane Tomlinson <stomlinson@mozilla.com>
2013-08-15 14:30:28 +0100

    fix IE11
    * update WinChan's ua sniff to handle IE11
    * remove the extra  after the  class.
    
    fixes #3593

--- resources/static/common/js/lib/winchan.js
+++ resources/static/common/js/lib/winchan.js
@@ -17,12 +17,19 @@
   // checking for IE8 or above
   function isInternetExplorer() {
     var rv = -1; // Return value assumes failure.
+    var ua = navigator.userAgent;
     if (navigator.appName === 'Microsoft Internet Explorer') {
-      var ua = navigator.userAgent;
       var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
       if (re.exec(ua) != null)
         rv = parseFloat(RegExp.$1);
     }
+    else if (ua.indexOf("Trident") > -1) {
+      var re = new RegExp("rv:([0-9]{2,2}[\.0-9]{0,})");
+      if (re.exec(ua) !== null) {
+        rv = parseFloat(RegExp.$1);
+      }
+    }
+
     return rv >= 8;
   }
 
