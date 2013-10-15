d181cef71e29669dfc450599fc0ed5581a80c0db
Shane Tomlinson <stomlinson@mozilla.com>
2013-10-15 19:29:10 +0100

    Add tests for loggedInUser type when calling navigator.id.watch

--- resources/static/test/cases/include.js
+++ resources/static/test/cases/include.js
@@ -1,10 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-(function() {
+(function(undefined) {
   "use strict";
 
+  var testHelpers = BrowserID.TestHelpers;
+
   module("include.js");
+
+  function noOp() {};
 
   test("navigator.id is available", function() {
     equal(typeof navigator.id, "object", "navigator.id namespace is available");
@@ -40,6 +44,75 @@
       ok(fails);
     });
   });
+
+  test("watch only accepts null, undefined, or a string for loggedInUser", function() {
+    var err;
+
+    // a string, null, and undefined are valid
+    try {
+      callWatch("strings@are.valid");
+    } catch(e) {
+      err = e;
+    }
+    testHelpers.testUndefined(err);
+
+    try {
+      callWatch(null);
+    } catch(e) {
+      err = e;
+    }
+    testHelpers.testUndefined(err);
+
+    try {
+      callWatch(undefined);
+    } catch(e) {
+      err = e;
+    }
+    testHelpers.testUndefined(err);
+
+    // a boolean, an object, an array, or a number are not.
+    try {
+      callWatch(false);
+    } catch(e) {
+      err = e;
+    }
+    ok(err);
+    err = undefined;
+
+    try {
+      callWatch({});
+    } catch(e) {
+      err = e;
+    }
+    ok(err);
+    err = undefined;
+
+    try {
+      callWatch([]);
+    } catch(e) {
+      err = e;
+    }
+    ok(err);
+    err = undefined;
+
+    try {
+      callWatch(1);
+    } catch(e) {
+      err = e;
+    }
+    ok(err);
+    err = undefined;
+
+  });
+
+  function callWatch(loggedInUser) {
+    navigator.id.watch({
+      loggedInUser: loggedInUser,
+      onlogin: noOp,
+      onlogout: noOp
+    });
+  }
+
 
 }());
 
