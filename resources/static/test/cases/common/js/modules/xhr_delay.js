e4fd97f124b2f303d10ad30360a9862c26e0c433
Khue Vu <khuevu@gmail.com>
2014-02-16 14:54:16 +0800

    Fix for issue #3270: The xhr_delay error message doesn't specify which WSAPI call is timing out

--- resources/static/test/cases/common/js/modules/xhr_delay.js
+++ resources/static/test/cases/common/js/modules/xhr_delay.js
@@ -37,6 +37,22 @@
     equal(testHelpers.delayVisible(), false, "slowXHR screen no longer visible");
   });
 
+  test("xhr_delay shows the delay screen with see more info link", function() {
+    var request = {
+      network: {
+        type: "GET", 
+        url: "/wsapi/session_context"
+      }
+    };
+    mediator.publish("xhr_delay", request)
+    ok($(".openMoreInfo:visible").length, "see more info link is visible");
+    $(".openMoreInfo").click();
+    ok($(".moreInfo").is(":visible"), "expanded info is visible");
+
+    mediator.publish("xhr_complete");
+    
+  });
+
   test("xhr_complete does not hide delay screen if delay screen not started by xhr_delay", function() {
 
     screens.delay.show("wait", {title: "test delay", message: "testing"});
