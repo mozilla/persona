mozilla/persona

commit 80a91d4054a1506e5389e795b7df5d695eb9fc9a

Split context into network-context and user-context

resources/static/common/js/models/network-context.js
@@ -2,11 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-BrowserID.Models.SessionContext = (function() {
+BrowserID.Models.NetworkContext = (function() {
   "use strict";
   /**
    * This model represents Session Context. It contains basic info
-   * about the user as well as info needed to communicate with the backend.
+   * needed to communicate with the backend.
    */
 
   var bid = BrowserID,
@@ -14,11 +14,6 @@ BrowserID.Models.SessionContext = (function() {
       sc;
 
   var Module = bid.Modules.Module.extend({
-    // user context - perhaps this should be somewhere else, like a user model?
-    userid: und,
-    auth_level: und,
-    has_password: und,
-    // network context
     csrf_token: und,
     local_time: und,
     domain_key_creation_time: und,
@@ -36,10 +31,6 @@ BrowserID.Models.SessionContext = (function() {
 
     setContext: function(context) {
       this.importFrom(context,
-        'userid',
-        'auth_level',
-        'has_password',
-
         'csrf_token',
         'server_time',
         'domain_key_creation_time',
@@ -53,30 +44,6 @@ BrowserID.Models.SessionContext = (function() {
       if (context.forceCookiesEnabled) {
         this.cookies = context.forceCookiesEnabled;
       }
-    },
-
-    getUserId: function() {
-      return this.userid;
-    },
-
-    setUserId: function(userId) {
-      this.userid = userId;
-    },
-
-    setAuthLevel: function(authLevel) {
-      this.auth_level = authLevel;
-    },
-
-    getAuthLevel: function() {
-      return this.auth_level || false;
-    },
-
-    isUserAuthenticated: function() {
-      return !!this.auth_level;
-    },
-
-    hasPassword: function() {
-      return !!this.has_password;
     },
 
     getCsrfToken: function() {

resources/static/common/js/modules/interaction_data.js
@@ -147,8 +147,8 @@ BrowserID.Modules.InteractionData = (function() {
     var self = this;
 
     model.publishCurrent(function(status) {
-      user.withContext(function(context) {
-        beginSampling.call(self, context);
+      user.withContext(function(userContext, networkContext) {
+        beginSampling.call(self, networkContext);
 
         var msg = status ? "interaction_data_send_complete"
                          : "interaction_data_send_error";
@@ -159,11 +159,11 @@ BrowserID.Modules.InteractionData = (function() {
     });
   }
 
-  function beginSampling(context) {
+  function beginSampling(networkContext) {
     /*jshint validthis: true */
     var self = this,
-        dataSampleRate = context.data_sample_rate,
-        serverTime = context.server_time;
+        dataSampleRate = networkContext.data_sample_rate,
+        serverTime = networkContext.server_time;
 
     // set the sample rate as defined by the server.  It's a value
     // between 0..1, integer or float, and it specifies the percentage

lib/static_resources.js
@@ -54,7 +54,8 @@ var common_js = [
   '/common/js/modules/xhr.js',
 
   '/common/js/models/models.js',
-  '/common/js/models/context.js',
+  '/common/js/models/user-context.js',
+  '/common/js/models/network-context.js',
   '/common/js/models/rp_info.js',
 
   '/common/js/network.js',
@@ -177,7 +178,8 @@ exports.resources = {
     '/common/js/modules/xhr.js',
 
     '/common/js/models/models.js',
-    '/common/js/models/context.js',
+    '/common/js/models/user-context.js',
+    '/common/js/models/network-context.js',
     '/common/js/models/rp_info.js',
     '/common/js/models/interaction_data.js',
 

resources/static/common/js/models/user-context.js
@@ -1,0 +1,67 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+BrowserID.Models.UserContext = (function() {
+  "use strict";
+  /**
+   * This model represents User Context. It contains basic info
+   * about the user.
+   */
+
+  var bid = BrowserID,
+      und,
+      sc;
+
+  var Module = bid.Modules.Module.extend({
+    // user context - perhaps this should be somewhere else, like a user model?
+    userid: und,
+    auth_level: und,
+    has_password: und,
+
+    init: function(options) {
+      var self = this;
+
+      self.setContext(options);
+
+      sc.init.call(self, options);
+    },
+
+    setContext: function(context) {
+      this.importFrom(context,
+        'userid',
+        'auth_level',
+        'has_password'
+        );
+    },
+
+    getUserId: function() {
+      return this.userid;
+    },
+
+    setUserId: function(userId) {
+      this.userid = userId;
+    },
+
+    setAuthLevel: function(authLevel) {
+      this.auth_level = authLevel;
+    },
+
+    getAuthLevel: function() {
+      return this.auth_level || false;
+    },
+
+    isUserAuthenticated: function() {
+      return !!this.auth_level;
+    },
+
+    hasPassword: function() {
+      return !!this.has_password;
+    }
+  });
+
+  sc = Module.sc;
+
+  return Module;
+}());
+

resources/static/common/js/network.js
@@ -11,7 +11,7 @@ BrowserID.Network = (function() {
       context,
       mediator = bid.Mediator,
       XHR = bid.Modules.XHR,
-      SessionContext = bid.Models.SessionContext,
+      NetworkContext = bid.Models.NetworkContext,
       xhr,
       // XXX get this out of here!
       storage = bid.Storage;
@@ -60,9 +60,9 @@ BrowserID.Network = (function() {
   }
 
   function setContext(newContext) {
-    context = SessionContext.create(newContext);
+    context = NetworkContext.create(newContext);
 
-    mediator.publish("context_info", context);
+    mediator.publish("context_info", newContext);
   }
 
   function clearContext() {

resources/static/common/js/user.js
@@ -11,6 +11,7 @@ BrowserID.User = (function() {
       helpers = bid.Helpers,
       mediator = bid.Mediator,
       cryptoLoader = bid.CryptoLoader,
+      UserContext = bid.Models.UserContext,
       User,
       pollTimeout,
       provisioning = bid.Provisioning,
@@ -80,9 +81,9 @@ BrowserID.User = (function() {
   }
 
   function removeInvalidIdentities(onSuccess, onFailure) {
-    withContext(function(context) {
-        var serverTime = context.getServerTime();
-        var creationTime = context.getDomainKeyCreationTime();
+    withContext(function(userContext, networkContext) {
+        var serverTime = networkContext.getServerTime();
+        var creationTime = networkContext.getDomainKeyCreationTime();
 
         cryptoLoader.load(function(jwcrypto) {
           var issuer = User.rpInfo.getIssuer();
@@ -335,14 +336,18 @@ BrowserID.User = (function() {
 
 
   function withContext(onSuccess, onFailure) {
-    network.withContext(onSuccess, onFailure);
+    network.withContext(function(networkContext) {
+      // the context object will have been updated in onContextChange.
+      onSuccess(context, networkContext);
+    }, onFailure);
   }
 
   function clearContext() {
     network.clearContext();
   }
 
-  function onContextChange(msg, context) {
+  function onContextChange(msg, newContext) {
+    context = UserContext.create(newContext);
     var authLevel = context.getAuthLevel();
     if (window.$) {
       // TODO get this out of here!
@@ -1289,8 +1294,8 @@ BrowserID.User = (function() {
         // we use the current time from the browserid servers
         // to avoid issues with clock drift on user's machine.
         // (issue #329)
-        withContext(function(context) {
-          var serverTime = context.getServerTime();
+        withContext(function(userContext, networkContext) {
+          var serverTime = networkContext.getServerTime();
           cryptoLoader.load(function(jwcrypto) {
             var sk = jwcrypto.loadSecretKeyFromObject(idInfo.priv);
 

resources/static/test/cases/common/js/user.js
@@ -62,8 +62,8 @@
     ok(typeof expires === "number" && !isNaN(expires), "expiration date is valid");
 
     // this should be based on server time, not local time.
-    lib.withContext(function(context) {
-      var time = context.getServerTime();
+    lib.withContext(function(userContext, networkContext) {
+      var time = networkContext.getServerTime();
       var nowPlus2Mins = time.getTime() + (2 * 60 * 1000);
 
       // expiration date must be within 5 seconds of 2 minutes from now - see

resources/views/test.ejs
@@ -149,7 +149,8 @@
     <script src="/common/js/modules/xhr.js"></script>
     <script src="/common/js/models/models.js"></script>
     <script src="/common/js/models/rp_info.js"></script>
-    <script src="/common/js/models/context.js"></script>
+    <script src="/common/js/models/user-context.js"></script>
+    <script src="/common/js/models/network-context.js"></script>
     <script src="/common/js/network.js"></script>
     <script src="/common/js/crypto-loader.js"></script>
     <script src="/common/js/provisioning.js"></script>

commit 89cd0feb64f6bbca873ea6f6f391919cc42b3337

fix a bug that effectively disabled KPI collection.
Bug was introduced in 80a91d4054a1506e5389e795b7df5d695eb9fc9a when NetworkContext and UserContext
were extracted from network.js. I forgot to add the `data_sample_rate` field.

resources/static/common/js/models/network-context.js
@@ -33,6 +33,7 @@ BrowserID.Models.NetworkContext = (function() {
       this.importFrom(context,
         'csrf_token',
         'server_time',
+        'data_sample_rate',
         'domain_key_creation_time',
         'code_version',
         'cookies',
@@ -54,6 +55,12 @@ BrowserID.Models.NetworkContext = (function() {
       if (!this.local_time) throw new Error("can't get local time!");
 
       return this.local_time;
+    },
+
+    getDataSampleRate: function () {
+      if (!this.data_sample_rate) throw new Error("can't get data sample rate!");
+
+      return this.data_sample_rate;
     },
 
     getServerTime: function() {

resources/static/common/js/modules/interaction_data.js
@@ -162,8 +162,8 @@ BrowserID.Modules.InteractionData = (function() {
   function beginSampling(networkContext) {
     /*jshint validthis: true */
     var self = this,
-        dataSampleRate = networkContext.data_sample_rate,
-        serverTime = networkContext.server_time;
+        dataSampleRate = networkContext.getDataSampleRate(),
+        serverTime = networkContext.getServerTime();
 
     // set the sample rate as defined by the server.  It's a value
     // between 0..1, integer or float, and it specifies the percentage