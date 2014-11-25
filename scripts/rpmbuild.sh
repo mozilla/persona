68d6cf1ceb5d778fd1aea938c5aabf7b1a72b0fa
Daniel Thornton <dthornton@mozilla.com>
2014-11-24 16:33:14 -0800

    fix build scripts to build persona-server.rpm and work on centos 7

--- scripts/rpmbuild.sh
+++ scripts/rpmbuild.sh
@@ -10,11 +10,11 @@ progname=$(basename $0)
 cd $(dirname $0)/..    # top level of the checkout
 
 mkdir -p rpmbuild/SOURCES rpmbuild/SPECS rpmbuild/SOURCES
-rm -rf rpmbuild/RPMS rpmbuild/SOURCES/browserid
+rm -rf rpmbuild/RPMS rpmbuild/SOURCES/persona
 
 tar --exclude rpmbuild --exclude .git --exclude .svn \
     --exclude var -czf \
-    "$PWD/rpmbuild/SOURCES/browserid-server.tar.gz" .
+    "$PWD/rpmbuild/SOURCES/persona-server.tar.gz" .
 
 set +e
 
@@ -22,12 +22,12 @@ export GIT_REVISION=$(git log -1 --oneline)
 export SVN_REVISION=$(svn info locale/ | sed -n -e "s,^Revision: ,,p")
 
 rpmbuild --define "_topdir $PWD/rpmbuild" \
-         --define "svnrev $SVN_REVISION" -ba scripts/browserid.spec
+         --define "svnrev $SVN_REVISION" -ba scripts/persona.spec
 rc=$?
 if [ $rc -eq 0 ]; then
     ls -l $PWD/rpmbuild/RPMS/*/*.rpm
 else
-    echo "$progname: failed to build browserid RPM (rpmbuild rc=$rc)" >&2
+    echo "$progname: failed to build persona RPM (rpmbuild rc=$rc)" >&2
 fi
 
 exit $rc
